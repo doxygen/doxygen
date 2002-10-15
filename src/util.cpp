6bc4cd0ea321e784be64022fea5013e1b181a593
Dimitri van Heesch <dimitri@stack.nl>
2002-10-15 16:41:03 +0000

    Fixed a typo

--- src/util.cpp
+++ src/util.cpp
@@ -3834,7 +3834,7 @@ void filterLatexString(QTextStream &t,const char *str,
                      if (Config_getBool("PDF_HYPERLINKS"))
                        t << "\\mbox{]}";
                      else if (insideItem)
-                       t << "\\[";
+                       t << "\\]";
                      else
                        t << "]";             
                      break;
