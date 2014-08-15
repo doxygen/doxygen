b272b4b5077873457a0f6b517ac799f4a5f8c951
Dimitri van Heesch <dimitri@stack.nl>
2014-08-15 11:19:55 +0200

    Bug 733722 - XML Output invalid: XML_PROGRAMLISTING=YES copies Unicode form feed character (U+000C) to XML files

--- src/util.cpp
+++ src/util.cpp
@@ -5605,6 +5605,11 @@ QCString convertToXML(const char *s)
       case '&':  growBuf.addStr("&amp;");  break;
       case '\'': growBuf.addStr("&apos;"); break; 
       case '"':  growBuf.addStr("&quot;"); break;
+      case  1: case  2: case  3: case  4: case  5: case  6: case  7: case  8:
+      case 11: case 12: case 13: case 14: case 15: case 16: case 17: case 18:
+      case 19: case 20: case 21: case 22: case 23: case 24: case 25: case 26:
+      case 27: case 28: case 29: case 30: case 31:
+        break; // skip invalid XML characters (see http://www.w3.org/TR/2000/REC-xml-20001006#NT-Char)
       default:   growBuf.addChar(c);       break;
     }
   }
