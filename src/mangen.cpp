1e8a69902a2cbb3118e6de5959223b9c8d3a843a
Dimitri van Heesch <dimitri@stack.nl>
2014-08-30 19:18:16 +0200

    Bug 735482 - [PATCH] Remove a not needed initialization of a local variable in src/mangen.cpp

--- src/mangen.cpp
+++ src/mangen.cpp
@@ -341,7 +341,7 @@ void ManGenerator::codify(const char *str)
                     break;
         case '\n':  t << "\n"; firstCol=TRUE; col=0; break;
         case '\\':  t << "\\"; col++; break;
-        case '\"':  c = '\''; // no break!
+        case '\"':  // no break!
         default:    p=writeUtf8Char(t,p-1); firstCol=FALSE; col++; break;
       }
     }
