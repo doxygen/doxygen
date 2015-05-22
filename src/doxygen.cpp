0971dc7771be1b386a204c3d11515f7d0f5af427
Adrian Negreanu <adrian.m.negreanu@intel.com>
2015-05-22 09:46:01 +0300

    specify that doxygen searches files in INPUT tag

--- src/doxygen.cpp
+++ src/doxygen.cpp
@@ -10857,7 +10857,7 @@ void searchInputFiles()
    *             Determine Input Files                                      *
    **************************************************************************/
 
-  g_s.begin("Searching for files to process...\n");
+  g_s.begin("Searching INPUT for files to process...\n");
   QDict<void> *killDict = new QDict<void>(10007);
   QStrList &inputList=Config_getList("INPUT");
   g_inputFiles.setAutoDelete(TRUE);
