eae07d978b524c787daeab911ca087b47a964577
Dimitri van Heesch <dimitri@stack.nl>
2015-05-31 17:12:09 +0200

    Added support for language codes

--- src/lang_cfg.py
+++
@@ -1,8 +0,0 @@
-import sys
-
-if (len(sys.argv) > 1):
-    if (sys.argv[1] == "ENONLY"):
-        print("#define ENGLISH_ONLY")
-    else:
-        for x in range(1, len(sys.argv)):
-            print("#define LANG_%s"%(sys.argv[x]))
