4137b3b0093ee544092011f99dc1c20d2632e64e
alexhoe <alexandra.hoefer@fau.de>
2016-07-04 10:31:25 +0200

    Add files via upload
    Improve inline formulas style

--- src/rtfdocvisitor.cpp
+++ src/rtfdocvisitor.cpp
@@ -493,17 +493,21 @@ void RTFDocVisitor::visit(DocFormula *f)
     if (m_lastIsPara) m_t << "\\par" << endl;
 
 	if (line) {
-		m_t << "\\pard { \\field\\flddirty {\\*\\fldinst  INCLUDEPICTURE \"";
+		m_t << "\\pard {\\field\\flddirty {\\*\\fldinst  INCLUDEPICTURE \"";
+		m_t << imgPath;
+		m_t << "\" \\\\d \\\\*MERGEFORMAT}{\\fldrslt Formula}}" << endl;
+		m_lastIsPara=TRUE;
+		m_t << "}" <<endl;
 	}
 	else
 	{
 		m_t << "\\pard \\qc { \\field\\flddirty {\\*\\fldinst  INCLUDEPICTURE \"";
+		m_t << imgPath;
+		m_t << "\" \\\\d \\\\*MERGEFORMAT}{\\fldrslt Formula}}" << endl;
+		m_t << "\\par" << endl;
+		m_lastIsPara=TRUE;
+		m_t << "}" <<endl;
 	}
-    m_t << imgPath;
-    m_t << "\" \\\\d \\\\*MERGEFORMAT}{\\fldrslt Formula}}" << endl;
-    m_t << "\\par" << endl;
-    m_lastIsPara=TRUE;
-	m_t << "}" <<endl;
 
   DBG_RTF("{\\comment RTFDocVisitor::visit(DocFormula)}\n");
   m_lastIsPara=FALSE;
