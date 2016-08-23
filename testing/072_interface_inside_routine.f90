b685fd166b60960a3351a4e21fb2d8f6913db8d1
Melven Roehrig-Zoellner <Melven.Roehrig-Zoellner@DLR.de>
2016-08-23 23:24:45 +0200

    Further fixes for interfaces inside procedures in Fortran

--- testing/072_interface_inside_routine.f90
+++ testing/072_interface_inside_routine.f90
@@ -1,13 +1,19 @@
-!// objective: don't show interfaces inside routines except for callgraphs
+!// objective: don't show interfaces inside routines except for arguments
 !// check: 072__interface__inside__routine_8f90.xml
+!// check: interfaceuse__tst_1_1interfarg.xml
 !// config: OPTIMIZE_FOR_FORTRAN=YES
 !// config: EXTRACT_ALL=YES
 
 !> Test for interface
-SUBROUTINE use_tst()
-
+SUBROUTINE use_tst(interfArg)
 INTERFACE
   SUBROUTINE routine_interface()
   END SUBROUTINE routine_interface
+  INTEGER FUNCTION interfArg(str)
+    CHARACTER(LEN=*), INTENT(IN) :: str
+  END FUNCTION
 END INTERFACE
+CONTAINS
+  SUBROUTINE inner_routine
+  END SUBROUTINE
 END SUBROUTINE use_tst
