1f1eb82179a5bbfe3ad8b76b3713be88db742c71
Josh Soref <jsoref@users.noreply.github.com>
2017-04-05 23:54:38 +0000

    spelling: display

--- src/diagram.cpp
+++ src/diagram.cpp
@@ -308,7 +308,7 @@ QCString DiagramItem::label() const
   QCString result;
   if (!templSpec.isEmpty())
   {
-    // we use classDef->name() here and not diplayName() in order
+    // we use classDef->name() here and not displayName() in order
     // to get the name used in the inheritance relation.
     QCString n = classDef->name();
     if (/*n.right(2)=="-g" ||*/ n.right(2)=="-p")
