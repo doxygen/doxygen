65a8d47b310167658579d5d0d7aa512be1c35696
Friedrich W. H. Kossebau <kossebau@kde.org>
2017-06-14 00:32:46 +0200

    Bug 783768 - Add QHP_NO_CONSTRUCTOR_KEYWORDS option
    Allows to avoid clashes between keywords for classes themselves and
    for their constructors methods

--- src/qhp.cpp
+++ src/qhp.cpp
@@ -226,6 +226,7 @@ void Qhp::addIndexItem(Definition *context,MemberDef *md,
   if (md) // member
   {
     static bool separateMemberPages = Config_getBool(SEPARATE_MEMBER_PAGES);
+    static bool noConstructorKeywords = Config_getBool(QHP_NO_CONSTRUCTOR_KEYWORDS);
     if (context==0) // global member
     {
       if (md->getGroupDef())
@@ -234,6 +235,7 @@ void Qhp::addIndexItem(Definition *context,MemberDef *md,
         context = md->getFileDef();
     }
     if (context==0) return; // should not happen
+    if (md->isConstructor() && noConstructorKeywords) return;
     QCString cfname  = md->getOutputFileBase();
     QCString cfiname = context->getOutputFileBase();
     QCString level1  = context->name();
