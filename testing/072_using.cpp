1c3317e72f7845f121009c10add4d209d607ce48
albert-github <albert.tests@gmail.com>
2018-04-01 19:52:57 +0200

    Test renumbering
    The tests numbers 067 and 068 were used twice, test 067 has been renamed to 072 and test 068 has been renamed to 073.

---
+++ testing/072_using.cpp
@@ -0,0 +1,7 @@
+// objective: test template parameters for a type alias
+// check: 072__using_8cpp.xml
+
+/** \file */
+
+/** @brief A vector */
+template<class T> using Vec = std::vector<T>;
---
+++ testing/073_typed_enum.cpp
@@ -0,0 +1,7 @@
+// objective: test underlying type and strongness for an enum
+// check: 073__typed__enum_8cpp.xml
+
+/** \file */
+
+/** @brief A strongly-typed enum */
+enum class E: unsigned short {};
