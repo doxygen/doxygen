92292de7230d98fbc307d02b4b0d4535f740aff3
Dimitri van Heesch <doxygen@gmail.com>
2019-04-28 14:39:54 +0200

    Replaced NULL by 0.

--- src/htmldocvisitor.cpp
+++ src/htmldocvisitor.cpp
@@ -790,7 +790,7 @@ void HtmlDocVisitor::visit(DocIncOperator *op)
     popEnabled();
     if (!m_hide) 
     {
-      FileDef *fd = NULL;
+      FileDef *fd = 0;
       if (!op->includeFileName().isEmpty())
       {
         QFileInfo cfi( op->includeFileName() );
