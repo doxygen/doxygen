34e55d6cec842543dcf4275079e2e897c9e9c080
Josh Soref <jsoref@users.noreply.github.com>
2019-11-12 00:41:43 -0500

    spelling: horizontal

--- libmscgen/gd.h
+++ libmscgen/gd.h
@@ -1499,7 +1499,7 @@ BGD_DECLARE(void) gdImageFlipHorizontal(gdImagePtr im);
 BGD_DECLARE(void) gdImageFlipVertical(gdImagePtr im);
 BGD_DECLARE(void) gdImageFlipBoth(gdImagePtr im);
 
-#define GD_FLIP_HORINZONTAL 1
+#define GD_FLIP_HORIZONTAL 1
 #define GD_FLIP_VERTICAL 2
 #define GD_FLIP_BOTH 3
 
