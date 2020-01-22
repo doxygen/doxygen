b68346aef1704dbfd5fe152b8b083d106ec5fccf
Michal Michalski <m.michalski2@partner.samsung.com>
2020-01-22 11:36:02 +0100

    [WebIDL] Add basic webidl code parser implementation.

---
+++ src/widlcode.h
@@ -0,0 +1,38 @@
+#ifndef DOXYGEN_WIDLCODE_H
+#define DOXYGEN_WIDLCODE_H
+
+#include "parserintf.h"
+
+
+class CodeOutputInterface;
+class FileDef;
+class MemberDef;
+class QCString;
+class Definition;
+
+
+class WidlCodeParser : public CodeParserInterface
+{
+  public:
+    void parseCode(CodeOutputInterface &codeOutIntf,
+                   const char *scopeName,
+                   const QCString &input,
+                   SrcLangExt lang,
+                   bool isExampleBlock,
+                   const char *exampleName=0,
+                   FileDef *fileDef=0,
+                   int startLine=-1,
+                   int endLine=-1,
+                   bool inlineFragment=FALSE,
+                   const MemberDef *memberDef=0,
+                   bool showLineNumbers=TRUE,
+                   const Definition *searchCtx=0,
+                   bool collectXrefs=TRUE
+                  ) {
+        codeOutInterface.codify(input);
+    }
+    void resetCodeParserState() {}
+};
+
+
+#endif
