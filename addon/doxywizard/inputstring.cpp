9b2564369d1769c6bce4695057c2b0184a1457e2
albert-github <albert.tests@gmail.com>
2020-02-14 15:44:45 +0100

    Doxywizard unknown configuration enum values
    Analogous to pull request #7586 a wrong value is given check e.g `OUTPUT_LANGUAGE        = dutch`

--- addon/doxywizard/inputstring.cpp
+++ addon/doxywizard/inputstring.cpp
@@ -253,3 +253,15 @@ void InputString::writeValue(QTextStream &t,QTextCodec *codec)
   writeStringValue(t,codec,m_str);
 }
 
+QString InputString::checkEnumVal(const QString &value)
+{
+  QString val = value.trimmed().toLower();
+  QStringList::Iterator it;
+  for ( it= m_values.begin(); it != m_values.end(); ++it )
+  {
+    QString enumVal = *it;
+    if (enumVal.toLower() == val) return enumVal;
+  }
+
+  return m_default;
+}
--- addon/doxywizard/inputstring.h
+++ addon/doxywizard/inputstring.h
@@ -50,6 +50,7 @@ class InputString : public QObject, public Input
     QVariant &value();
     void update();
     Kind kind() const { return String; }
+    StringMode stringMode() const { return m_sm; }
     QString docs() const { return m_docs; }
     QString id() const { return m_id; }
     QString templateDocs() const { return m_tdocs; }
@@ -59,6 +60,7 @@ class InputString : public QObject, public Input
     void writeValue(QTextStream &t,QTextCodec *codec);
     void setTemplateDocs(const QString &docs) { m_tdocs = docs; }
     bool isEmpty() { return m_str.isEmpty(); }
+    QString checkEnumVal(const QString &value);
 
   public slots:
     void reset();
