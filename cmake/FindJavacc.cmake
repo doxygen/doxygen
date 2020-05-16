23d09616f425649f71e6ea367c860c902daf1144
albert-github <albert.tests@gmail.com>
2020-05-16 12:57:07 +0200

    issue #7778 Build fails with javacc 5.0
    At the moment doxygen works with javaCC version 7.0.5 but no test is done for the version. The minimum requirement has been set to 7.0.5
    javacc does not have an option to retrieve the version though when giving `--version` a bit more information is given, this information is filtered.
    (an issue has been submitted to javacc to obtain the version information: https://github.com/javacc/javacc/issues/172).

--- cmake/FindJavacc.cmake
+++ cmake/FindJavacc.cmake
@@ -4,6 +4,20 @@ mark_as_advanced(JAVACC_EXECUTABLE)
 if(JAVACC_EXECUTABLE)
   set(JAVACC_FOUND 1)
   message(STATUS "The javacc executable: ${JAVACC_EXECUTABLE}")
+  if(JAVACC_EXECUTABLE)
+    execute_process(
+      COMMAND "${JAVACC_EXECUTABLE}" --version
+      OUTPUT_VARIABLE JAVACC_TEMP_VERSION
+      OUTPUT_STRIP_TRAILING_WHITESPACE
+      RESULT_VARIABLE _JavaCC_version_result
+    )
+    if(_JavaCC_version_result)
+      message(WARNING "Unable to determine JavaCC version: ${_JavaCC_version_result}")
+      set(JAVACC_FOUND 0)
+    else()
+      string(REGEX REPLACE ".*([0-9]+\.[0-9]\.[0-9]+).*" "\\1" JAVACC_VERSION ${JAVACC_TEMP_VERSION})
+    endif()
+  endif()
 else()
   set(JAVACC_FOUND 0)
   message(STATUS "The javacc executable not found, using existing files")
