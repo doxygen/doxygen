c21e21640a049bcda5bef690a272a8cb1483bcc9
Dimitri van Heesch <doxygen@gmail.com>
2020-05-31 20:02:52 +0200

    Added copyright header to threadpool.h

--- src/threadpool.h
+++ src/threadpool.h
@@ -1,3 +1,18 @@
+/******************************************************************************
+ *
+ * Copyright (C) 1997-2020 by Dimitri van Heesch.
+ *
+ * Permission to use, copy, modify, and distribute this software and its
+ * documentation under the terms of the GNU General Public License is hereby
+ * granted. No representations are made about the suitability of this software
+ * for any purpose. It is provided "as is" without express or implied warranty.
+ * See the GNU General Public License for more details.
+ *
+ * Documents produced by Doxygen are derivative works derived from the
+ * input used in their production; they are not affected by this license.
+ *
+ */
+
 #ifndef THREADPOOL_H
 #define THREADPOOL_H
 
@@ -105,6 +120,7 @@ class ThreadPool
     // thread-safe triggered queue of tasks:
     std::mutex m_mutex;
     std::condition_variable m_cond;
+
     // note that a packaged_task<void> can store a packaged_task<R>:
     std::deque< std::packaged_task<void()> > m_work;
 
