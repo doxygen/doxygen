b711b21a041b14d7b2fddea610d904671f260924
Costa Shulyupin <costa@MakeLinux.net>
2020-06-08 15:04:32 +0300

    refactoring: split DotNode::writeLabel from DotNode::writeBox
    to allow further generic development
    
    It is challenging to review the code because of reordered code:
    Actually code of labCol calculation is unchanged.

--- src/dotnode.h
+++ src/dotnode.h
@@ -81,6 +81,7 @@ class DotNode
     void writeXML(TextStream &t,bool isClassGraph) const;
     void writeDocbook(TextStream &t,bool isClassGraph) const;
     void writeDEF(TextStream &t) const;
+    void writeLabel(TextStream &t, GraphType gt) const;
     void writeBox(TextStream &t,GraphType gt,GraphOutputFormat f,
                   bool hasNonReachableChildren) const;
     void writeArrow(TextStream &t,GraphType gt,GraphOutputFormat f,const DotNode *cn,
