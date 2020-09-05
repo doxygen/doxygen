05547d571d1f5c32e7577ef26c830a240505d0aa
Dimitri van Heesch <doxygen@gmail.com>
2020-09-05 19:17:07 +0200

    Refactoring: making pycode.l reentrant

--- src/pycode.h
+++ src/pycode.h
@@ -1,12 +1,10 @@
 /******************************************************************************
  *
- * 
- *
- * Copyright (C) 1997-2015 by Dimitri van Heesch.
+ * Copyright (C) 1997-2020 by Dimitri van Heesch.
  *
  * Permission to use, copy, modify, and distribute this software and its
- * documentation under the terms of the GNU General Public License is hereby 
- * granted. No representations are made about the suitability of this software 
+ * documentation under the terms of the GNU General Public License is hereby
+ * granted. No representations are made about the suitability of this software
  * for any purpose. It is provided "as is" without express or implied warranty.
  * See the GNU General Public License for more details.
  *
@@ -36,6 +34,8 @@ class Definition;
 class PythonCodeParser : public CodeParserInterface
 {
   public:
+    PythonCodeParser();
+    virtual ~PythonCodeParser();
     void parseCode(CodeOutputInterface &codeOutIntf,
                    const char *scopeName,
                    const QCString &input,
@@ -52,6 +52,9 @@ class PythonCodeParser : public CodeParserInterface
                    bool collectXrefs=TRUE
                   );
     void resetCodeParserState();
+  private:
+    struct Private;
+    std::unique_ptr<Private> p;
 };
 
 
