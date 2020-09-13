576c85b15afa630f8734d68243258f1c307fb844
Dimitri van Heesch <doxygen@gmail.com>
2020-09-13 21:12:32 +0200

    Force first word in table cell to hyphenate
    Added invisible space before first word in every table cell to allow wraps.
    http://sigstp.blogspot.com/2010/09/latex-hyphenate-single-long-words-in.html
    
    Force first word in table cell to hyphenate
    
    Added invisible space before first word in every table cell to allow wraps.
    http://sigstp.blogspot.com/2010/09/latex-hyphenate-single-long-words-in.html
    
    Force first word in table cell to hyphenate
    
    Added invisible space before first word in every table cell to allow wraps.
    http://sigstp.blogspot.com/2010/09/latex-hyphenate-single-long-words-in.html

--- src/latexdocvisitor.cpp
+++ src/latexdocvisitor.cpp
@@ -1284,6 +1284,10 @@ void LatexDocVisitor::visitPre(DocHtmlCell *c)
   {
     m_t << "\\cellcolor{\\tableheadbgcolor}\\textbf{ ";
   }
+  else 
+  {
+    if (c->isFirst()) m_t << "\\hspace{0pt}";
+  }
   if (cs>1)
   {
     setCurrentColumn(currentColumn()+cs-1);
@@ -1307,7 +1311,11 @@ void LatexDocVisitor::visitPost(DocHtmlCell *c)
     setInColSpan(FALSE);
     m_t << "}";
   }
-  if (!c->isLast()) m_t << "&";
+  if (!c->isLast()) {
+    m_t << "&";
+    if (!c->isHeading()) m_t << "\\hspace{0pt}";
+  }
+
 }
 
 void LatexDocVisitor::visitPre(DocInternal *)
