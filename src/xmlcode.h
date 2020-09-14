0fc06d657d596adcc289a5f228973ea268efd66d
Dimitri van Heesch <doxygen@gmail.com>
2020-09-14 21:13:58 +0200

    Refactoring: making xmlcode.l reentrant

--- src/xmlcode.h
+++ src/xmlcode.h
@@ -1,12 +1,10 @@
 /******************************************************************************
  *
- * 
- *
- * Copyright (C) 1997-2014 by Dimitri van Heesch.
+ * Copyright (C) 1997-2020 by Dimitri van Heesch.
  *
  * Permission to use, copy, modify, and distribute this software and its
- * documentation under the terms of the GNU General Public License is hereby 
- * granted. No representations are made about the suitability of this software 
+ * documentation under the terms of the GNU General Public License is hereby
+ * granted. No representations are made about the suitability of this software
  * for any purpose. It is provided "as is" without express or implied warranty.
  * See the GNU General Public License for more details.
  *
@@ -32,6 +30,8 @@ class Definition;
 class XMLCodeParser : public CodeParserInterface
 {
   public:
+    XMLCodeParser();
+    virtual ~XMLCodeParser();
     void parseCode(CodeOutputInterface &codeOutIntf,
                    const char *scopeName,
                    const QCString &input,
@@ -48,6 +48,9 @@ class XMLCodeParser : public CodeParserInterface
                    bool collectXRefs=TRUE
                   );
     void resetCodeParserState();
+  private:
+    struct Private;
+    std::unique_ptr<Private> p;
 };
 
 
