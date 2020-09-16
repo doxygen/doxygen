e7311a036e16cf56692862103be37df95853a1dc
Anonymous Maarten <anonymous.maarten@gmail.com>
2020-09-16 16:24:06 +0200

    Allow building doxygen as a subproject

--- cmake/Coverage.cmake
+++ cmake/Coverage.cmake
@@ -7,23 +7,23 @@ if(enable_coverage)
       CACHE INTERNAL "")
   add_custom_target(coverage-clean
 	COMMAND ${LCOV_PATH} --rc lcov_branch_coverage=1 --directory . --zerocounters
-	WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
+	WORKING_DIRECTORY ${PROJECT_BINARY_DIR}
   )
   add_custom_target(coverage
 	COMMAND ${LCOV_PATH} --rc lcov_branch_coverage=1 --directory . --capture --output-file cov.info
 	COMMAND ${LCOV_PATH} --rc lcov_branch_coverage=1 --remove cov.info '*/c++/*' '*/_ctype.h' '*/generated_src/*' --output-file cov.info.cleaned
-	COMMAND ${CMAKE_COMMAND} -Dsearch=${CMAKE_BINARY_DIR}
-	                         -Dreplace=${CMAKE_SOURCE_DIR}
+	COMMAND ${CMAKE_COMMAND} -Dsearch=${PROJECT_BINARY_DIR}
+	                         -Dreplace=${PROJECT_SOURCE_DIR}
 				 -Dsrc=cov.info.cleaned
 				 -Ddst=cov.info.final
-				 -P ${CMAKE_SOURCE_DIR}/cmake/SearchReplace.cmake
+				 -P ${PROJECT_SOURCE_DIR}/cmake/SearchReplace.cmake
 	COMMAND ${GENHTML_PATH} --rc genhtml_branch_coverage=1
 	                        --function-coverage --branch-coverage
 	                        --title "Doxygen Coverage Report" --num-spaces 2
-				--legend --prefix ${CMAKE_SOURCE_DIR} --demangle-cpp
+				--legend --prefix ${PROJECT_SOURCE_DIR} --demangle-cpp
 				--output-directory cov_output cov.info.final
         COMMAND ${CMAKE_COMMAND} -E remove cov.info cov.info.cleaned cov.info.final
-	WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
+	WORKING_DIRECTORY ${PROJECT_BINARY_DIR}
   )
   add_custom_command(TARGET coverage POST_BUILD
 	COMMAND ;
