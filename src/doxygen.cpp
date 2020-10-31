doxygen/doxygen

commit d03e8d9411ab3e983fc3413c147fba1a5e5c9dad

Refactoring: modernize TooltipManager class and source reference lists
- Tooltips are now collected per file instead of globally
- Source reference lists now use STL containers

src/doxygen.cpp
@@ -3709,59 +3709,10 @@ static void transferFunctionReferences()
             )
          ) /* match found */
       {
-        MemberSDict *defDict = mdef->getReferencesMembers();
-        MemberSDict *decDict = mdec->getReferencesMembers();
-        if (defDict!=0)
-        {
-          MemberSDict::IteratorDict msdi(*defDict);
-          MemberDef *rmd;
-          for (msdi.toFirst();(rmd=msdi.current());++msdi)
-          {
-            if (decDict==0 || decDict->find(rmd->name())==0)
-            {
-              mdec->addSourceReferences(rmd);
-            }
-          }
-        }
-        if (decDict!=0)
-        {
-          MemberSDict::IteratorDict msdi(*decDict);
-          MemberDef *rmd;
-          for (msdi.toFirst();(rmd=msdi.current());++msdi)
-          {
-            if (defDict==0 || defDict->find(rmd->name())==0)
-            {
-              mdef->addSourceReferences(rmd);
-            }
-          }
-        }
-
-        defDict = mdef->getReferencedByMembers();
-        decDict = mdec->getReferencedByMembers();
-        if (defDict!=0)
-        {
-          MemberSDict::IteratorDict msdi(*defDict);
-          MemberDef *rmd;
-          for (msdi.toFirst();(rmd=msdi.current());++msdi)
-          {
-            if (decDict==0 || decDict->find(rmd->name())==0)
-            {
-              mdec->addSourceReferencedBy(rmd);
-            }
-          }
-        }
-        if (decDict!=0)
-        {
-          MemberSDict::IteratorDict msdi(*decDict);
-          MemberDef *rmd;
-          for (msdi.toFirst();(rmd=msdi.current());++msdi)
-          {
-            if (defDict==0 || defDict->find(rmd->name())==0)
-            {
-              mdef->addSourceReferencedBy(rmd);
-            }
-          }
-        }
+        mdef->mergeReferences(mdec);
+        mdec->mergeReferences(mdef);
+        mdef->mergeReferencedBy(mdec);
+        mdec->mergeReferencedBy(mdef);
       }
     }
   }
@@ -7651,7 +7602,6 @@ static void generateFileSources()
           results.emplace_back(threadPool.queue(processFile));
         }
       }
-      // first wait until all files are processed
       for (auto &f : results)
       {
         auto ctx = f.get();
@@ -7855,6 +7805,32 @@ static void sortMemberLists()
   for (gli.toFirst();(gd=gli.current());++gli)
   {
     gd->sortMemberLists();
+  }
+}
+
+//----------------------------------------------------------------------------
+
+void computeTooltipTexts()
+{
+  QDictIterator<DefinitionIntf> di(*Doxygen::symbolMap);
+  DefinitionIntf *intf;
+  for (;(intf=di.current());++di)
+  {
+    if (intf->definitionType()==DefinitionIntf::TypeSymbolList) // list of symbols
+    {
+      DefinitionListIterator dli(*(DefinitionList*)intf);
+      Definition *d;
+      // for each symbol
+      for (dli.toFirst();(d=dli.current());++dli)
+      {
+        d->computeTooltip();
+      }
+    }
+    else // single symbol
+    {
+      Definition *d = (Definition *)intf;
+      if (d!=Doxygen::globalScope) d->computeTooltip();
+    }
   }
 }
 
@@ -11441,6 +11417,10 @@ void parseInput()
 
   g_s.begin("Correcting members for VHDL...\n");
   vhdlCorrectMemberProperties();
+  g_s.end();
+
+  g_s.begin("Computing tooltip texts...\n");
+  computeTooltipTexts();
   g_s.end();
 
   if (Config_getBool(SORT_GROUP_NAMES))

addon/doxyparse/doxyparse.cpp
@@ -49,7 +49,7 @@
 class Doxyparse : public CodeOutputInterface
 {
   public:
-    Doxyparse(FileDef *fd) : m_fd(fd) {}
+    Doxyparse(const FileDef *fd) : m_fd(fd) {}
    ~Doxyparse() {}
 
     // these are just null functions, they can be used to produce a syntax highlighted
@@ -84,7 +84,7 @@ class Doxyparse : public CodeOutputInterface
     }
 
   private:
-    FileDef *m_fd;
+    const FileDef *m_fd;
 };
 
 static bool is_c_code = true;
@@ -110,7 +110,7 @@ static void findXRefSymbols(FileDef *fd)
   delete parse;
 }
 
-static bool ignoreStaticExternalCall(MemberDef *context, MemberDef *md) {
+static bool ignoreStaticExternalCall(const MemberDef *context, const MemberDef *md) {
   if (md->isStatic()) {
     if(md->getFileDef() && context->getFileDef()) {
       if(md->getFileDef()->getOutputFileBase() == context->getFileDef()->getOutputFileBase())
@@ -174,11 +174,11 @@ static void printReferenceTo(std::string type, std::string signature, std::strin
   printf("                type: %s\n", type.c_str());
   printf("                defined_in: \"%s\"\n", unescapeCharsInString(defined_in.c_str()).data());
 }
-static void printNumberOfConditionalPaths(MemberDef* md) {
+static void printNumberOfConditionalPaths(const MemberDef* md) {
   printf("          conditional_paths: %d\n", md->numberOfFlowKeyWords());
 }
 
-static int isPartOfCStruct(MemberDef * md) {
+static int isPartOfCStruct(const MemberDef * md) {
   return is_c_code && md->getClassDef() != NULL;
 }
 
@@ -198,7 +198,7 @@ std::string argumentData(const Argument &argument) {
   return data;
 }
 
-std::string functionSignature(MemberDef* md) {
+std::string functionSignature(const MemberDef* md) {
   std::string signature = sanitizeString(md->name().data());
   if(md->isFunction()){
     const ArgumentList &argList = md->argumentList();
@@ -215,7 +215,7 @@ std::string functionSignature(MemberDef* md) {
   return signature;
 }
 
-static void referenceTo(MemberDef* md) {
+static void referenceTo(const MemberDef* md) {
   std::string type = md->memberTypeName().data();
   std::string defined_in = "";
   std::string signature = "";
@@ -253,17 +253,17 @@ void protectionInformation(Protection protection) {
   }
 }
 
-void cModule(ClassDef* cd) {
-  MemberList* ml = cd->getMemberList(MemberListType_variableMembers);
+void cModule(const ClassDef* cd) {
+  const MemberList* ml = cd->getMemberList(MemberListType_variableMembers);
   if (ml) {
-    FileDef *fd = cd->getFileDef();
-    MemberList *fd_ml = fd->getMemberList(MemberListType_allMembersList);
+    const FileDef *fd = cd->getFileDef();
+    const MemberList *fd_ml = fd->getMemberList(MemberListType_allMembersList);
     if (!fd_ml || fd_ml->count() == 0) {
       printModule(fd->getOutputFileBase().data());
       printDefines();
     }
     MemberListIterator mli(*ml);
-    MemberDef* md;
+    const MemberDef* md;
     for (mli.toFirst(); (md=mli.current()); ++mli) {
       printDefinition("variable", cd->name().data() + std::string("::") + md->name().data(), md->getDefLine());
       protectionInformation(md->protection());
@@ -271,7 +271,7 @@ void cModule(ClassDef* cd) {
   }
 }
 
-static bool checkOverrideArg(const ArgumentList &argList, MemberDef *md) {
+static bool checkOverrideArg(const ArgumentList &argList, const MemberDef *md) {
   if(!md->isFunction() || argList.empty()){
     return false;
   }
@@ -285,7 +285,7 @@ static bool checkOverrideArg(const ArgumentList &argList, MemberDef *md) {
   return false;
 }
 
-void functionInformation(MemberDef* md) {
+void functionInformation(const MemberDef* md) {
   std::string temp = "";
   int size = md->getEndBodyLine() - md->getStartBodyLine() + 1;
   printNumberOfLines(size);
@@ -301,12 +301,10 @@ void functionInformation(MemberDef* md) {
   }
 
   printNumberOfConditionalPaths(md);
-  MemberSDict *defDict = md->getReferencesMembers();
-  if (defDict) {
-    MemberSDict::Iterator msdi(*defDict);
-    MemberDef *rmd;
+  auto refList = md->getReferencesMembers();
+  if (!refList.empty()) {
     printUses();
-    for (msdi.toFirst(); (rmd=msdi.current()); ++msdi) {
+    for (const auto &rmd : refList) {
       if (rmd->definitionType() == Definition::TypeMember && !ignoreStaticExternalCall(md, rmd) && !checkOverrideArg(argList, rmd)) {
         referenceTo(rmd);
       }
@@ -314,15 +312,15 @@ void functionInformation(MemberDef* md) {
   }
 }
 
-void prototypeInformation(MemberDef* md) {
+void prototypeInformation(const MemberDef* md) {
   printPrototypeYes();
   const ArgumentList &argList = md->argumentList();
   printNumberOfArguments(argList.size());
 }
 
-static void lookupSymbol(Definition *d) {
+static void lookupSymbol(const Definition *d) {
   if (d->definitionType() == Definition::TypeMember) {
-    MemberDef *md = dynamic_cast<MemberDef*>(d);
+    const MemberDef *md = dynamic_cast<const MemberDef*>(d);
     std::string type = md->memberTypeName().data();
     std::string signature = functionSignature(md);
     printDefinition(type, signature, md->getDefLine());
@@ -336,17 +334,17 @@ static void lookupSymbol(Definition *d) {
   }
 }
 
-void listMembers(MemberList *ml) {
+void listMembers(const MemberList *ml) {
   if (ml) {
     MemberListIterator mli(*ml);
-    MemberDef *md;
+    const MemberDef *md;
     for (mli.toFirst(); (md=mli.current()); ++mli) {
       lookupSymbol((Definition*) md);
     }
   }
 }
 
-void listAllMembers(ClassDef* cd) {
+void listAllMembers(const ClassDef* cd) {
   // methods
   listMembers(cd->getMemberList(MemberListType_functionMembers));
   // constructors
@@ -355,7 +353,7 @@ void listAllMembers(ClassDef* cd) {
   listMembers(cd->getMemberList(MemberListType_variableMembers));
 }
 
-static void classInformation(ClassDef* cd) {
+static void classInformation(const ClassDef* cd) {
   if (is_c_code) {
     cModule(cd);
   } else {
@@ -418,7 +416,7 @@ static void listSymbols() {
       ClassSDict *classes = fd->getClassSDict();
       if (classes) {
         ClassSDict::Iterator cli(*classes);
-        ClassDef *cd;
+        const ClassDef *cd;
         for (cli.toFirst(); (cd = cli.current()); ++cli) {
           if (!cd->isVisited()) {
             classInformation(cd);

src/clangparser.cpp
@@ -133,6 +133,7 @@ class ClangTUParser::Private
     CXToken *tokens = 0;
     uint numTokens = 0;
     StringVector filesInSameTU;
+    TooltipManager tooltipManager;
 
     // state while parsing sources
     MemberDef  *currentMemberDef=0;
@@ -572,7 +573,7 @@ void ClangTUParser::writeMultiLineCodeLink(CodeOutputInterface &ol,
                   const char *text)
 {
   static bool sourceTooltips = Config_getBool(SOURCE_TOOLTIPS);
-  TooltipManager::instance()->addTooltip(d);
+  p->tooltipManager.addTooltip(d);
   QCString ref  = d->getReference();
   QCString file = d->getOutputFileBase();
   QCString anchor = d->anchor();

src/code.l
@@ -175,6 +175,7 @@ struct codeYY_state
   QCString        forceTagReference;
   VariableContext theVarContext;
   CallContext     theCallContext;
+  TooltipManager  tooltipManager;
 };
 
 static bool isCastKeyword(const QCString &s);
@@ -241,7 +242,7 @@ static void addVariable(yyscan_t yyscanner,QCString type,QCString name);
 //-------------------------------------------------------------------
 
 static std::mutex g_searchIndexMutex;
-static std::mutex g_tooltipMutex;
+static std::mutex g_docCrossReferenceMutex;
 
 /* -----------------------------------------------------------------
  */
@@ -2471,10 +2472,7 @@ static void writeMultiLineCodeLink(yyscan_t yyscanner,CodeOutputInterface &ol,
 {
   struct yyguts_t *yyg = (struct yyguts_t*)yyscanner;
   bool sourceTooltips = Config_getBool(SOURCE_TOOLTIPS);
-  {
-    std::lock_guard<std::mutex> lock(g_tooltipMutex);
-    TooltipManager::instance()->addTooltip(d);
-  }
+  yyextra->tooltipManager.addTooltip(d);
   QCString ref  = d->getReference();
   QCString file = d->getOutputFileBase();
   QCString anchor = d->anchor();
@@ -2764,6 +2762,7 @@ static bool getLinkInScope(yyscan_t yyscanner,
         if (yyextra->currentDefinition && yyextra->currentMemberDef &&
             md!=yyextra->currentMemberDef && yyextra->insideBody && yyextra->collectXRefs)
         {
+          std::lock_guard<std::mutex> lock(g_docCrossReferenceMutex);
           addDocCrossReference(yyextra->currentMemberDef,const_cast<MemberDef*>(md));
         }
         //printf("d->getReference()='%s' d->getOutputBase()='%s' name='%s' member name='%s'\n",d->getReference().data(),d->getOutputFileBase().data(),d->name().data(),md->name().data());
@@ -2924,6 +2923,7 @@ static void generateClassOrGlobalLink(yyscan_t yyscanner,
       if (d && d->isLinkable() && md->isLinkable() &&
           yyextra->currentMemberDef && yyextra->collectXRefs)
       {
+        std::lock_guard<std::mutex> lock(g_docCrossReferenceMutex);
         addDocCrossReference(yyextra->currentMemberDef,const_cast<MemberDef*>(md));
       }
     }
@@ -2978,6 +2978,7 @@ static void generateClassOrGlobalLink(yyscan_t yyscanner,
           addToSearchIndex(yyscanner,clName);
           if (yyextra->currentMemberDef && yyextra->collectXRefs)
           {
+            std::lock_guard<std::mutex> lock(g_docCrossReferenceMutex);
             addDocCrossReference(yyextra->currentMemberDef,const_cast<MemberDef*>(md));
           }
           return;
@@ -3038,6 +3039,7 @@ static bool generateClassMemberLink(yyscan_t yyscanner,
       if (yyextra->currentDefinition && yyextra->currentMemberDef &&
           /*xmd!=yyextra->currentMemberDef &&*/ yyextra->insideBody && yyextra->collectXRefs)
       {
+        std::lock_guard<std::mutex> lock(g_docCrossReferenceMutex);
         addDocCrossReference(yyextra->currentMemberDef,xmd);
       }
 
@@ -3457,6 +3459,7 @@ static void writeObjCMethodCall(yyscan_t yyscanner,ObjCCallCtx *ctx)
               writeMultiLineCodeLink(yyscanner,*yyextra->code,ctx->method,name);
               if (yyextra->currentMemberDef && yyextra->collectXRefs)
               {
+                std::lock_guard<std::mutex> lock(g_docCrossReferenceMutex);
                 addDocCrossReference(yyextra->currentMemberDef,const_cast<MemberDef*>(ctx->method));
               }
             }
@@ -3531,6 +3534,7 @@ static void writeObjCMethodCall(yyscan_t yyscanner,ObjCCallCtx *ctx)
               writeMultiLineCodeLink(yyscanner,*yyextra->code,ctx->objectVar,object);
               if (yyextra->currentMemberDef && yyextra->collectXRefs)
               {
+                std::lock_guard<std::mutex> lock(g_docCrossReferenceMutex);
                 addDocCrossReference(yyextra->currentMemberDef,const_cast<MemberDef*>(ctx->objectVar));
               }
             }
@@ -3903,6 +3907,8 @@ void CCodeParser::parseCode(CodeOutputInterface &od,const char *className,const 
     delete yyextra->sourceFileDef;
     yyextra->sourceFileDef=0;
   }
+  // write the tooltips
+  yyextra->tooltipManager.writeTooltips(od);
 
   printlex(yy_flex_debug, FALSE, __FILE__, fd ? fd->fileName().data(): NULL);
   return;

src/context.cpp
@@ -4943,7 +4943,7 @@ class MemberContext::Private : public DefinitionContext<MemberContext::Private>
       Cachable &cache = getCache();
       if (!cache.sourceRefs)
       {
-        cache.sourceRefs.reset(MemberListContext::alloc(m_memberDef->getReferencesMembers(),TRUE));
+        cache.sourceRefs.reset(MemberListContext::alloc(m_memberDef->getReferencesMembers()));
       }
       return cache.sourceRefs.get();
     }
@@ -4952,7 +4952,7 @@ class MemberContext::Private : public DefinitionContext<MemberContext::Private>
       Cachable &cache = getCache();
       if (!cache.sourceRefBys)
       {
-        cache.sourceRefBys.reset(MemberListContext::alloc(m_memberDef->getReferencedByMembers(),TRUE));
+        cache.sourceRefBys.reset(MemberListContext::alloc(m_memberDef->getReferencedByMembers()));
       }
       return cache.sourceRefBys.get();
     }
@@ -8581,7 +8581,7 @@ TemplateListIntf::ConstIterator *InheritanceListContext::createIterator() const
 class MemberListContext::Private : public GenericNodeListContext
 {
   public:
-    void addMember(MemberDef *md)
+    void addMember(const MemberDef *md)
     {
       append(MemberContext::alloc(md));
     }
@@ -8628,6 +8628,16 @@ MemberListContext::MemberListContext(MemberSDict *list,bool doSort) : RefCounted
       p->addMember(md);
     }
   }
+}
+
+MemberListContext::MemberListContext(std::vector<const MemberDef *> &&ml) : RefCountedContext("MemberListContext")
+{
+  p = new Private;
+  for (const auto &md : ml)
+  {
+    p->addMember(md);
+  }
+  ml.clear();
 }
 
 MemberListContext::~MemberListContext()

src/context.h
@@ -954,6 +954,8 @@ class MemberListContext : public RefCountedContext, public TemplateListIntf
     { return new MemberListContext(ml); }
     static MemberListContext *alloc(MemberSDict *ml,bool doSort)
     { return new MemberListContext(ml,doSort); }
+    static MemberListContext *alloc(std::vector<const MemberDef *> &&ml)
+    { return new MemberListContext(std::move(ml)); }
 
     // TemplateListIntf
     virtual uint count() const;
@@ -966,6 +968,7 @@ class MemberListContext : public RefCountedContext, public TemplateListIntf
     MemberListContext();
     MemberListContext(const MemberList *ml);
     MemberListContext(MemberSDict *ml,bool doSort);
+    MemberListContext(std::vector<const MemberDef *> &&ml);
    ~MemberListContext();
     class Private;
     Private *p;

src/defgen.cpp
@@ -243,71 +243,58 @@ void generateDEFForMember(MemberDef *md,
 
   //printf("md->getReferencesMembers()=%p\n",md->getReferencesMembers());
 
-  MemberSDict *mdict = md->getReferencesMembers();
-  if (mdict)
+  QCString refPrefix = "  " + memPrefix + "ref-";
+  auto refList = md->getReferencesMembers();
+  for (const auto &rmd : refList)
   {
-    MemberSDict::Iterator mdi(*mdict);
-    MemberDef *rmd;
-    QCString refPrefix = "  " + memPrefix + "ref-";
-
-    for (mdi.toFirst();(rmd=mdi.current());++mdi)
+    if (rmd->getStartBodyLine()!=-1 && rmd->getBodyDef())
     {
-      if (rmd->getStartBodyLine()!=-1 && rmd->getBodyDef())
+      t << memPrefix << "referenceto = {" << endl;
+      t << refPrefix << "id = '"
+        << rmd->getBodyDef()->getOutputFileBase()
+        << "_1"   // encoded ':' character (see util.cpp:convertNameToFile)
+        << rmd->anchor() << "';" << endl;
+
+      t << refPrefix << "line = '"
+        << rmd->getStartBodyLine() << "';" << endl;
+
+      QCString scope = rmd->getScopeString();
+      QCString name = rmd->name();
+      if (!scope.isEmpty() && scope!=def->name())
       {
-        t << memPrefix << "referenceto = {" << endl;
-        t << refPrefix << "id = '"
-          << rmd->getBodyDef()->getOutputFileBase()
-          << "_1"   // encoded ':' character (see util.cpp:convertNameToFile)
-          << rmd->anchor() << "';" << endl;
-
-        t << refPrefix << "line = '"
-          << rmd->getStartBodyLine() << "';" << endl;
-
-        QCString scope = rmd->getScopeString();
-        QCString name = rmd->name();
-        if (!scope.isEmpty() && scope!=def->name())
-        {
-          name.prepend(scope+"::");
-        }
-
-        t << refPrefix << "name = ";
-        writeDEFString(t,name);
-        t << ';' << endl << "    };" << endl;
+        name.prepend(scope+"::");
       }
-    } /* for (mdi.toFirst...) */
+
+      t << refPrefix << "name = ";
+      writeDEFString(t,name);
+      t << ';' << endl << "    };" << endl;
+    }
   }
-  mdict = md->getReferencedByMembers();
-  if (mdict)
+  auto refByList = md->getReferencedByMembers();
+  for (const auto &rmd : refByList)
   {
-    MemberSDict::Iterator mdi(*mdict);
-    MemberDef *rmd;
-    QCString refPrefix = "  " + memPrefix + "ref-";
-
-    for (mdi.toFirst();(rmd=mdi.current());++mdi)
+    if (rmd->getStartBodyLine()!=-1 && rmd->getBodyDef())
     {
-      if (rmd->getStartBodyLine()!=-1 && rmd->getBodyDef())
+      t << memPrefix << "referencedby = {" << endl;
+      t << refPrefix << "id = '"
+        << rmd->getBodyDef()->getOutputFileBase()
+        << "_1"   // encoded ':' character (see util.cpp:convertNameToFile)
+        << rmd->anchor() << "';" << endl;
+
+      t << refPrefix << "line = '"
+        << rmd->getStartBodyLine() << "';" << endl;
+
+      QCString scope = rmd->getScopeString();
+      QCString name = rmd->name();
+      if (!scope.isEmpty() && scope!=def->name())
       {
-        t << memPrefix << "referencedby = {" << endl;
-        t << refPrefix << "id = '"
-          << rmd->getBodyDef()->getOutputFileBase()
-          << "_1"   // encoded ':' character (see util.cpp:convertNameToFile)
-          << rmd->anchor() << "';" << endl;
-
-        t << refPrefix << "line = '"
-          << rmd->getStartBodyLine() << "';" << endl;
-
-        QCString scope = rmd->getScopeString();
-        QCString name = rmd->name();
-        if (!scope.isEmpty() && scope!=def->name())
-        {
-          name.prepend(scope+"::");
-        }
-
-        t << refPrefix << "name = ";
-        writeDEFString(t,name);
-        t << ';' << endl << "    };" << endl;
+        name.prepend(scope+"::");
       }
-    } /* for (mdi.toFirst...) */
+
+      t << refPrefix << "name = ";
+      writeDEFString(t,name);
+      t << ';' << endl << "    };" << endl;
+    }
   }
 
   t << "    }; /* " << Prefix << "-member */" << endl;

src/definition.cpp
@@ -16,6 +16,10 @@
  */
 
 #include <algorithm>
+#include <iterator>
+#include <unordered_map>
+#include <string>
+
 #include <ctype.h>
 #include <qregexp.h>
 #include "md5.h"
@@ -58,8 +62,8 @@ class DefinitionImpl::IMPL
 
     SectionRefs sectionRefs;
 
-    MemberSDict *sourceRefByDict = 0;
-    MemberSDict *sourceRefsDict = 0;
+    std::unordered_map<std::string,const MemberDef *> sourceRefByDict;
+    std::unordered_map<std::string,const MemberDef *> sourceRefsDict;
     RefItemVector xrefListItems;
     GroupList *partOfGroups = 0;
 
@@ -100,8 +104,6 @@ class DefinitionImpl::IMPL
 
 DefinitionImpl::IMPL::~IMPL()
 {
-  delete sourceRefByDict;
-  delete sourceRefsDict;
   delete partOfGroups;
   delete brief;
   delete details;
@@ -138,8 +140,8 @@ void DefinitionImpl::IMPL::init(const char *df, const char *n)
   details         = 0;
   body            = 0;
   inbodyDocs      = 0;
-  sourceRefByDict = 0;
-  sourceRefsDict  = 0;
+  sourceRefByDict.clear();
+  sourceRefsDict.clear();
   outerScope      = Doxygen::globalScope;
   partOfGroups    = 0;
   hidden          = FALSE;
@@ -314,33 +316,11 @@ DefinitionImpl::DefinitionImpl(const DefinitionImpl &d)
 {
   m_impl = new DefinitionImpl::IMPL;
   *m_impl = *d.m_impl;
-  m_impl->sourceRefByDict = 0;
-  m_impl->sourceRefsDict = 0;
   m_impl->partOfGroups = 0;
   m_impl->brief = 0;
   m_impl->details = 0;
   m_impl->body = 0;
   m_impl->inbodyDocs = 0;
-  if (d.m_impl->sourceRefByDict)
-  {
-    m_impl->sourceRefByDict = new MemberSDict;
-    MemberSDict::IteratorDict it(*d.m_impl->sourceRefByDict);
-    MemberDef *md;
-    for (it.toFirst();(md=it.current());++it)
-    {
-      m_impl->sourceRefByDict->append(it.currentKey(),md);
-    }
-  }
-  if (d.m_impl->sourceRefsDict)
-  {
-    m_impl->sourceRefsDict = new MemberSDict;
-    MemberSDict::IteratorDict it(*d.m_impl->sourceRefsDict);
-    MemberDef *md;
-    for (it.toFirst();(md=it.current());++it)
-    {
-      m_impl->sourceRefsDict->append(it.currentKey(),md);
-    }
-  }
   if (d.m_impl->partOfGroups)
   {
     GroupListIterator it(*d.m_impl->partOfGroups);
@@ -1262,11 +1242,27 @@ void DefinitionImpl::writeInlineCode(OutputList &ol,const char *scopeName) const
   ol.popGeneratorState();
 }
 
+static inline std::vector<const MemberDef*> refMapToVector(const std::unordered_map<std::string,const MemberDef *> &map)
+{
+  // convert map to a vector of values
+  std::vector<const MemberDef *> result;
+  std::transform(map.begin(),map.end(),      // iterate over map
+                 std::back_inserter(result), // add results to vector
+                 [](const auto &item)
+                 { return item.second; }     // extract value to add from map Key,Value pair
+                );
+  // and sort it
+  std::sort(result.begin(),result.end(),
+              [](const auto &m1,const auto &m2) { return genericCompareMembers(m1,m2)<0; });
+  return result;
+}
+
 /*! Write a reference to the source code fragments in which this
  *  definition is used.
  */
 void DefinitionImpl::_writeSourceRefList(OutputList &ol,const char *scopeName,
-    const QCString &text,MemberSDict *members,bool /*funcOnly*/) const
+    const QCString &text,const std::unordered_map<std::string,const MemberDef *> &membersMap,
+    bool /*funcOnly*/) const
 {
   static bool latexSourceCode = Config_getBool(LATEX_SOURCE_CODE);
   static bool docbookSourceCode   = Config_getBool(DOCBOOK_PROGRAMLISTING);
@@ -1274,15 +1270,15 @@ void DefinitionImpl::_writeSourceRefList(OutputList &ol,const char *scopeName,
   static bool sourceBrowser   = Config_getBool(SOURCE_BROWSER);
   static bool refLinkSource   = Config_getBool(REFERENCES_LINK_SOURCE);
   ol.pushGeneratorState();
-  if (members)
+  if (!membersMap.empty())
   {
-    members->sort();
+    auto members = refMapToVector(membersMap);
 
     ol.startParagraph("reference");
     ol.parseText(text);
     ol.docify(" ");
 
-    QCString ldefLine=theTranslator->trWriteList((int)members->count());
+    QCString ldefLine=theTranslator->trWriteList((int)members.size());
 
     QRegExp marker("@[0-9]+");
     uint index=0;
@@ -1294,7 +1290,7 @@ void DefinitionImpl::_writeSourceRefList(OutputList &ol,const char *scopeName,
       bool ok;
       ol.parseText(ldefLine.mid(index,(uint)newIndex-index));
       uint entryIndex = ldefLine.mid(newIndex+1,matchLen-1).toUInt(&ok);
-      MemberDef *md=members->at(entryIndex);
+      const MemberDef *md=members.at(entryIndex);
       if (ok && md)
       {
         QCString scope=md->getScopeString();
@@ -1466,14 +1462,7 @@ void DefinitionImpl::addSourceReferencedBy(const MemberDef *md)
       name.prepend(scope+"::");
     }
 
-    if (m_impl->sourceRefByDict==0)
-    {
-      m_impl->sourceRefByDict = new MemberSDict;
-    }
-    if (m_impl->sourceRefByDict->find(name)==0)
-    {
-      m_impl->sourceRefByDict->append(name,md);
-    }
+    m_impl->sourceRefByDict.insert({name.str(),md});
   }
 }
 
@@ -1489,14 +1478,7 @@ void DefinitionImpl::addSourceReferences(const MemberDef *md)
       name.prepend(scope+"::");
     }
 
-    if (m_impl->sourceRefsDict==0)
-    {
-      m_impl->sourceRefsDict = new MemberSDict;
-    }
-    if (m_impl->sourceRefsDict->find(name)==0)
-    {
-      m_impl->sourceRefsDict->append(name,md);
-    }
+    m_impl->sourceRefsDict.insert({name.str(),md});
   }
 }
 
@@ -1960,29 +1942,20 @@ QCString DefinitionImpl::briefDescription(bool abbr) const
          QCString("");
 }
 
+void DefinitionImpl::computeTooltip()
+{
+  if (m_impl->brief && m_impl->brief->tooltip.isEmpty() && !m_impl->brief->doc.isEmpty())
+  {
+    const MemberDef *md = definitionType()==TypeMember ? dynamic_cast<const MemberDef*>(this) : 0;
+    const Definition *scope = definitionType()==TypeMember ? getOuterScope() : this;
+    m_impl->brief->tooltip = parseCommentAsText(scope,md,
+                                m_impl->brief->doc, m_impl->brief->file, m_impl->brief->line);
+  }
+}
+
 QCString DefinitionImpl::briefDescriptionAsTooltip() const
 {
-  if (m_impl->brief)
-  {
-    if (m_impl->brief->tooltip.isEmpty() && !m_impl->brief->doc.isEmpty())
-    {
-      static bool reentering=FALSE;
-      if (!reentering)
-      {
-        const MemberDef *md = definitionType()==TypeMember ? dynamic_cast<const MemberDef*>(this) : 0;
-        const Definition *scope = definitionType()==TypeMember ? getOuterScope() : this;
-        reentering=TRUE; // prevent requests for tooltips while parsing a tooltip
-        m_impl->brief->tooltip = parseCommentAsText(
-            scope,md,
-            m_impl->brief->doc,
-            m_impl->brief->file,
-            m_impl->brief->line);
-        reentering=FALSE;
-      }
-    }
-    return m_impl->brief->tooltip;
-  }
-  return QCString("");
+  return m_impl->brief ? m_impl->brief->tooltip : QCString();
 }
 
 int DefinitionImpl::briefLine() const
@@ -2100,15 +2073,48 @@ Definition *DefinitionImpl::getOuterScope() const
   return m_impl->outerScope;
 }
 
-MemberSDict *DefinitionImpl::getReferencesMembers() const
+std::vector<const MemberDef*> DefinitionImpl::getReferencesMembers() const
 {
-  return m_impl->sourceRefsDict;
+  return refMapToVector(m_impl->sourceRefsDict);
 }
 
-MemberSDict *DefinitionImpl::getReferencedByMembers() const
+std::vector<const MemberDef*> DefinitionImpl::getReferencedByMembers() const
 {
-  return m_impl->sourceRefByDict;
+  return refMapToVector(m_impl->sourceRefByDict);
 }
+
+void DefinitionImpl::mergeReferences(const Definition *other)
+{
+  const DefinitionImpl *defImpl = dynamic_cast<const DefinitionImpl*>(other);
+  if (defImpl)
+  {
+    for (const auto &kv : defImpl->m_impl->sourceRefsDict)
+    {
+      auto it = m_impl->sourceRefsDict.find(kv.first);
+      if (it != m_impl->sourceRefsDict.end())
+      {
+        m_impl->sourceRefsDict.insert(kv);
+      }
+    }
+  }
+}
+
+void DefinitionImpl::mergeReferencedBy(const Definition *other)
+{
+  const DefinitionImpl *defImpl = dynamic_cast<const DefinitionImpl*>(other);
+  if (defImpl)
+  {
+    for (const auto &kv : defImpl->m_impl->sourceRefByDict)
+    {
+      auto it = m_impl->sourceRefByDict.find(kv.first);
+      if (it != m_impl->sourceRefByDict.end())
+      {
+        m_impl->sourceRefByDict.insert({kv.first,kv.second});
+      }
+    }
+  }
+}
+
 
 void DefinitionImpl::setReference(const char *r)
 {

commit 42d60bd97de0d1fbf85fdc9117e8dd46a026db1b

issue 8143: copydoc in excluded namespace broken after d03e8d9

src/doxygen.cpp
@@ -7810,11 +7810,21 @@ static void sortMemberLists()
 
 //----------------------------------------------------------------------------
 
-void computeTooltipTexts()
+static bool isSymbolHidden(const Definition *d)
+{
+  bool hidden = d->isHidden();
+  const Definition *parent = d->getOuterScope();
+  return parent ? hidden || isSymbolHidden(parent) : hidden;
+}
+
+static void computeTooltipTexts()
 {
   for (const auto &kv : Doxygen::symbolMap)
   {
-    kv.second->computeTooltip();
+    if (!isSymbolHidden(kv.second))
+    {
+      kv.second->computeTooltip();
+    }
   }
 }
 