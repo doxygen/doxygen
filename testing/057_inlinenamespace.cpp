a32a778635bef381022c90e3944bedfca539d6a0
albert-github <albert.tests@gmail.com>
2020-11-22 13:25:09 +0100

    Doxygen warnings are not seen as errors during running tests
    Corrected warnings of test 57.

--- testing/057_inlinenamespace.cpp
+++ testing/057_inlinenamespace.cpp
@@ -2,18 +2,36 @@
 // check: namespacelibrary.xml
 // check: namespacelibrary_1_1v1.xml
 // check: namespacelibrary_1_1v2.xml
+
+/// the main namespace
 namespace library
 {
+    /// the first namespace
     namespace v1
     {
-        class foo { public: void member(); };
+        /// the class
+        class foo {
+          public:
+            /// member of the class
+            void member();
+        };
+        /// a method
         void func();
+        /// a namespace
         namespace NS {}
     }
+    /// the inline namespace
     inline namespace v2
     {
-        class foo { public: void member(); };
+        /// the class
+        class foo {
+          public:
+            /// member of the class
+            void member();
+        };
+        /// a method
         void func();
+        /// a namespace
         namespace NS {}
     }
 }
