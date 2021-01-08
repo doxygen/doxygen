ef9b20ac7f8a8621fcfc299f8bd0b80422390f4b
Dimitri van Heesch <doxygen@gmail.com>
2021-01-08 12:58:14 +0100

    Update doxygen copyright (year)

--- src/doxygen.cpp
+++ src/doxygen.cpp
@@ -10088,7 +10088,7 @@ static void devUsage()
 
 static void usage(const char *name,const char *versionString)
 {
-  msg("Doxygen version %s\nCopyright Dimitri van Heesch 1997-2019\n\n",versionString);
+  msg("Doxygen version %s\nCopyright Dimitri van Heesch 1997-2021\n\n",versionString);
   msg("You can use doxygen in a number of ways:\n\n");
   msg("1) Use doxygen to generate a template configuration file:\n");
   msg("    %s [-s] -g [configName]\n\n",name);
