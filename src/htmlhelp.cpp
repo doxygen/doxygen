7fc52b556d6c1925a3b16052762e084368a95427
albert-github <albert.tests@gmail.com>
2022-04-06 17:55:36 +0200

    bug 783768 QCH files: classes & their constructors get conflicting keyword handling
    The problem in not only present in the Qt help (qhp / qch) but also in the HTML Help (chm).
    All items with the same name are put onto one heap. For thi index this is corrected by adding the function signatures, analogous to the javascript search functionality of doxygen.

--- src/htmlhelp.cpp
+++ src/htmlhelp.cpp
@@ -591,9 +591,10 @@ void HtmlHelp::addIndexItem(const Definition *context,const MemberDef *md,
     if (context==0) return; // should not happen
 
     QCString cfname  = md->getOutputFileBase();
+    QCString argStr  = md->argsString();
     QCString cfiname = context->getOutputFileBase();
     QCString level1  = context->name();
-    QCString level2  = md->name();
+    QCString level2  = md->name() + argStr;
     QCString contRef = separateMemberPages ? cfname : cfiname;
     QCString memRef  = cfname;
     QCString anchor  = !sectionAnchor.isEmpty() ? sectionAnchor : md->anchor();
