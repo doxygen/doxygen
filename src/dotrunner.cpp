07205fda53b5cbf86e7f469bc6beb2c3b55f94e6
udaken <7896698+udaken@users.noreply.github.com>
2022-05-31 14:15:29 +0900

    Changed dot tool launch method
    On Windows platforms, prevented conhost from starting when starting dot tool.

--- src/dotrunner.cpp
+++ src/dotrunner.cpp
@@ -197,7 +197,7 @@ bool DotRunner::run()
       srcFile = m_jobs.front().srcFile;
       srcLine = m_jobs.front().srcLine;
     }
-    if ((exitCode=Portable::system(m_dotExe,dotArgs,FALSE))!=0) goto error;
+    if ((exitCode=Portable::system(m_dotExe,dotArgs,true))!=0) goto error;
   }
   else
   {
@@ -206,7 +206,7 @@ bool DotRunner::run()
       srcFile = s.srcFile;
       srcLine = s.srcLine;
       dotArgs=QCString("\"")+m_file+"\" "+s.args;
-      if ((exitCode=Portable::system(m_dotExe,dotArgs,FALSE))!=0) goto error;
+      if ((exitCode=Portable::system(m_dotExe,dotArgs,true))!=0) goto error;
     }
   }
 
@@ -222,7 +222,7 @@ bool DotRunner::run()
       {
         if (!resetPDFSize(width,height,getBaseNameOfOutput(s.output))) goto error;
         dotArgs=QCString("\"")+m_file+"\" "+s.args;
-        if ((exitCode=Portable::system(m_dotExe,dotArgs,FALSE))!=0) goto error;
+        if ((exitCode=Portable::system(m_dotExe,dotArgs,true))!=0) goto error;
       }
     }
 
