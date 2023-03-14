673fde98deed248bd2d766e898ebafe638d8dd68
Christian Eichert <102165928+ecxod@users.noreply.github.com>
2023-03-14 15:43:18 +0100

    A Lax is a Fish, and not a Cookie
    Cookie “doxygen_width” does not have a proper “SameSite” attribute value. Soon, cookies without the “SameSite” attribute or with an invalid value will be treated as “Lax”. This means that the cookie will no longer be sent in third-party contexts. If your application depends on this cookie being available in such contexts, please add the “SameSite=None“ attribute to it. To know more about the “SameSite“ attribute, read https://developer.mozilla.org/docs/Web/HTTP/Headers/Set-Cookie/SameSite

--- templates/html/resize.js
+++ templates/html/resize.js
@@ -59,7 +59,8 @@ function initResizable()
       var date = new Date();
       date.setTime(date.getTime()+(10*365*24*60*60*1000)); // default expiration is one week
       expiration = date.toGMTString();
-      document.cookie = cookie_namespace + "_" + cookie + "=" + val + "; SameSite=Lax; expires=" + expiration+"; path=/";
+      document.cookie = `${cookie_namespace}_${cookie}=${val}; SameSite=Strict; Secure; expires=${expiration}; path=/`;
+
     }
   }
 
