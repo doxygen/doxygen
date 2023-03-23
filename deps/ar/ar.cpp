4ab76c9cb2dd3eaee98aaf88e745176bf2854f90
sven <sven2000@student.miun.se>
2023-03-23 18:01:55 +0100

    Second function partially done

--- deps/ar/ar.cpp
+++ deps/ar/ar.cpp
@@ -10,19 +10,74 @@ using std::string;
 using std::vector;
 
 using Dictionary = vector<string>;
+using LongForm = vector<string>;
+using Lmatch = vector<string>;
+using nonDictWords = vector<string>;
+using Phi = function<int(string)>;
+using C = function<void(void)>;
+
+LongForm expand_known_abbr(nonDictWords nonDictWords, Dictionary D) {
+  LongForm retvec;
+
+  return retvec;
+}
+
+int vowels(string word) {
+  return 0;
+}
+
+int consonants(string word) {
+  return 0;
+}
+
+
+Lmatch split_matching(string ident, vector<Dictionary> D);
+vector<string> string_matching(std::string token, Dictionary D,
+                               function<int(string)> phi,
+                               function<void(void)> cost);
+
+LongForm expansion_matching(nonDictWords nonDictWords, Dictionary D) {
+  LongForm retvec = expand_known_abbr(nonDictWords, D);
+
+  LongForm toExpand;
+  std::copy_if(nonDictWords.begin(), nonDictWords.end(), toExpand, [&retvec](string const value){
+      return std::find(retvec.begin(), retvec.end(), value) == retvec.end();
+  });
+
+  Phi phi = nullptr;
+  C c = nullptr;
+  for(auto token: toExpand) {
+    for(auto dictionary: D) {
+      if(vowels(token) > consonants(token)) {
+        phi = [&D]( string token ){ return 0; };
+      } else {
+        phi = [&D]( string token ){ return 0; };
+      }
+      Lmatch matches = string_matching(token, D, phi, c);
+      if(matches.size() > 0) {
+        std::copy_if(matches.begin(), matches.end(), retvec, [&retvec](string const value){
+            return std::find(retvec.begin(), retvec.end(), value) == retvec.end();
+        });
+        break;
+      }
+    }
+  }
+
+  return retvec;
+}
 
 vector<string> string_matching(std::string token, Dictionary D,
                                function<int(string)> phi,
                                function<void(void)> cost) {
   vector<string> retvec;
+
   return retvec;
 }
-void expansion_matching();
 
-vector<string> split_matching(string ident, vector<Dictionary> D) {
-  vector<string> retvec;
+Lmatch split_matching(string ident, vector<Dictionary> D) {
+  Lmatch retvec;
 
-  function<int(string)> phi = [&D](string word) {
+  Phi phi = [&D](string word) {
     bool found = true;
     for(auto dict : D) {
       if(std::find(dict.begin(), dict.end(), word) == dict.end()) {
@@ -31,7 +86,7 @@ vector<string> split_matching(string ident, vector<Dictionary> D) {
     }
     return found;
   };
-  function<void(void)> cost = []() {};
+  C cost = []() {};
 
   for (auto dict : D) {
     retvec = string_matching(ident, dict, phi, cost);
@@ -41,7 +96,5 @@ vector<string> split_matching(string ident, vector<Dictionary> D) {
   }
   return retvec;
 }
-
-void expansion_matching() {}
 
 } // namespace ar
