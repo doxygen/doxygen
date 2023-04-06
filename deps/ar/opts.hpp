3c8531a73ff7cd0e04f3a3b291a911d0c5b2f1db
sven <sven2000@student.miun.se>
2023-04-06 20:26:41 +0200

    Some minor cleanup

--- deps/ar/opts.hpp
+++ deps/ar/opts.hpp
@@ -1,7 +1,15 @@
 #ifndef OPTS_HPP
 #define OPTS_HPP
 
+#define INFO_LEVEL 0
+#define WARNING_LEVEL 1
+#define RESULT_LEVEL 2
+
+#define LOG_LEVEL WARNING_LEVEL
+
 #include <functional>
+#include <iomanip>
+#include <iostream>
 #include <string>
 #include <unordered_map>
 #include <vector>
@@ -18,13 +26,28 @@ using Cfunc = std::function<int(std::string &)>;
 #define YELLO_COLOR "\033[33m"
 #define RESET_COLOR "\033[0m"
 
+#if LOG_LEVEL <= INFO_LEVEL
 #define INFO(message)                                                          \
-  std::cout << YELLO_COLOR << std::setw(9) << std::left << "[INFO] " << message << RESET_COLOR << std::endl;
+  std::cout << YELLO_COLOR << std::setw(9) << std::left << "[INFO] "           \
+            << message << RESET_COLOR << std::endl;
+#else
+#define INFO(message)
+#endif
 
-#define WARNING(message)                                                          \
-  std::cout << REDDD_COLOR << std::setw(9) << std::left << "[WARNING] " << message << RESET_COLOR << std::endl;
+#if LOG_LEVEL <= WARNING_LEVEL
+#define WARNING(message)                                                       \
+  std::cout << REDDD_COLOR << std::setw(9) << std::left << "[WARNING] "        \
+            << message << RESET_COLOR << std::endl;
+#else
+#define WARNING(message)
+#endif
 
-#define RESULT(message)                                                          \
-  std::cout << GREEN_COLOR << std::setw(9) << std::left << "[OUTPUT] " << message << RESET_COLOR << std::endl;
+#if LOG_LEVEL <= RESULT_LEVEL
+#define RESULT(message)                                                        \
+  std::cout << GREEN_COLOR << std::setw(9) << std::left << "[OUTPUT] "         \
+            << message << RESET_COLOR << std::endl;
+#else
+#define RESULT(message)
+#endif
 
 #endif
