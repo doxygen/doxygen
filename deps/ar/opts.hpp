d3c34fc96001feaf5336482c190623c8a541a4c8
sven <sven2000@student.miun.se>
2023-04-06 21:13:57 +0200

    Made som smaller structural changes

--- deps/ar/opts.hpp
+++ deps/ar/opts.hpp
@@ -2,8 +2,9 @@
 #define OPTS_HPP
 
 #define INFO_LEVEL 0
-#define WARNING_LEVEL 1
-#define RESULT_LEVEL 2
+#define RESULT_LEVEL 1
+#define WARNING_LEVEL 2
+#define DANGER_LEVEL 3
 
 #define LOG_LEVEL WARNING_LEVEL
 
@@ -24,28 +25,37 @@ using Cfunc = std::function<int(std::string &)>;
 #define REDDD_COLOR "\033[31m"
 #define GREEN_COLOR "\033[32m"
 #define YELLO_COLOR "\033[33m"
+#define BLUER_COLOR "\033[34m"
 #define RESET_COLOR "\033[0m"
 
-#if LOG_LEVEL <= INFO_LEVEL
-#define INFO(message)                                                          \
-  std::cout << YELLO_COLOR << std::setw(9) << std::left << "[INFO] "           \
-            << message << RESET_COLOR << std::endl;
+enum log_levels { INFO, RESULT, WARNING, DANGER };
+
+void LOG(std::string message, log_levels loglevel);
+
+#define PRINT(color, type, message)                                            \
+  std::cout << color << std::setw(9) << std::left << type << message           \
+            << RESET_COLOR << std::endl;
+
+#if LOG_LEVEL == INFO_LEVEL
+#define INFO(message) PRINT(BLUER_COLOR, "[INFO] ", message)
 #else
 #define INFO(message)
 #endif
 
+#if LOG_LEVEL <= RESULT_LEVEL
+#define RESULT(message) PRINT(GREEN_COLOR, "[OUTPUT] ", message)
+#else
+#define RESULT(message)
+#endif
+
 #if LOG_LEVEL <= WARNING_LEVEL
-#define WARNING(message)                                                       \
-  std::cout << REDDD_COLOR << std::setw(9) << std::left << "[WARNING] "        \
-            << message << RESET_COLOR << std::endl;
+#define WARNING(message) PRINT(YELLO_COLOR, "[WARNING] ", message)
 #else
 #define WARNING(message)
 #endif
 
-#if LOG_LEVEL <= RESULT_LEVEL
-#define RESULT(message)                                                        \
-  std::cout << GREEN_COLOR << std::setw(9) << std::left << "[OUTPUT] "         \
-            << message << RESET_COLOR << std::endl;
+#if LOG_LEVEL <= DANGER_LEVEL
+#define DANGER(message) PRINT(REDDD_COLOR, "[DANGER] ", message)
 #else
 #define RESULT(message)
 #endif
