a7bfc9ddd23b853835f0ce1989fdf6750316500c
sven <sven2000@student.miun.se>
2023-04-06 17:46:34 +0200

    It is not behaving the same all the time

--- deps/ar/opts.hpp
+++ deps/ar/opts.hpp
@@ -14,13 +14,17 @@ using Phi = std::function<int(std::string &, std::string &)>;
 using Cfunc = std::function<int(std::string &)>;
 
 #define REDDD_COLOR "\033[31m"
+#define GREEN_COLOR "\033[32m"
 #define YELLO_COLOR "\033[33m"
-#define RESET_COLOR "\033[30m"
+#define RESET_COLOR "\033[0m"
 
 #define INFO(message)                                                          \
-  std::cout << YELLO_COLOR << std::setw(10) << "[INFO] " << message << RESET_COLOR << std::endl;
+  std::cout << YELLO_COLOR << std::setw(9) << std::left << "[INFO] " << message << RESET_COLOR << std::endl;
 
 #define WARNING(message)                                                          \
-  std::cout << REDDD_COLOR << std::setw(10) << "[WARNING] " << message << RESET_COLOR << std::endl;
+  std::cout << REDDD_COLOR << std::setw(9) << std::left << "[WARNING] " << message << RESET_COLOR << std::endl;
+
+#define RESULT(message)                                                          \
+  std::cout << GREEN_COLOR << std::setw(9) << std::left << "[OUTPUT] " << message << RESET_COLOR << std::endl;
 
 #endif
