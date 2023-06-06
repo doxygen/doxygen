1d50a167d8657419ebd8864327dd1280637c1864
albert-github <albert.tests@gmail.com>
2023-06-06 16:40:34 +0200

    issue #10104 Incorrect handling of unicode characters
    Correcting code in respect to differences in char type between Java (i.e. char is unsigned) and C++ (char is signed)
    (see also https://github.com/javacc/javacc/pull/265)

--- vhdlparser/vhdl_adj.py
+++ vhdlparser/vhdl_adj.py
@@ -14,12 +14,34 @@
 
 import sys
 
-def main():
+def adjust_tm():
+    inputFile = open(sys.argv[2], 'r')
+    outputFile = open(sys.argv[3], 'w')
+    for line in inputFile:
+        if "if (curChar < 64)" in line:
+          outputFile.write(line.replace("if (curChar < 64)","if (curChar >= 0 && curChar < 64)"))
+        elif "else if (curChar < 128)" in line:
+          outputFile.write(line.replace("else if (curChar < 128)","else if (curChar >= 0 && curChar < 128)"))
+        elif "int hiByte = (curChar >> 8);" in line:
+          outputFile.write("         unsigned char curUChar;\n")
+          outputFile.write("         curUChar = *((unsigned char *)&curChar);\n")
+          outputFile.write(line.replace("int hiByte = (curChar >> 8);","int hiByte = (curUChar >> 8);"))
+        elif "int i2 = (curChar & 0xff) >> 6;" in line:
+          outputFile.write(line.replace("int i2 = (curChar & 0xff) >> 6;","int i2 = (curUChar & 0xff) >> 6;"))
+        elif "unsigned long long l2 = 1ULL << (curChar & 077);" in line:
+          outputFile.write(line.replace("unsigned long long l2 = 1ULL << (curChar & 077);","unsigned long long l2 = 1ULL << (curUChar & 077);"))
+        else:
+          outputFile.write(line)
+
+def adjust_parser():
     inputFile = open(sys.argv[1], 'r')
     outputFile = open(sys.argv[2], 'w')
     for line in inputFile:
         outputFile.write(line.replace("assert(false);","assert(false);return QCString();"))
 
 if __name__ == '__main__':
-    main()
+    if sys.argv[1] == '--tm':
+      adjust_tm()
+    else:
+      adjust_parser()
 
