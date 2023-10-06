7087e782d448d78ec256fa605518ff3a3a5a524e
Dimitri van Heesch <doxygen@gmail.com>
2023-10-06 19:52:29 +0200

    Added missing start of paragraph after code fragment

--- src/htmldocvisitor.cpp
+++ src/htmldocvisitor.cpp
@@ -779,6 +779,7 @@ void HtmlDocVisitor::operator()(const DocInclude &inc)
                                        inc.type()==DocInclude::SnippetTrimLeft
                                       );
       m_ci.endCodeFragment("DoxyCode");
+      forceStartParagraph(inc);
       break;
     case DocInclude::SnippetDoc:
     case DocInclude::IncludeDoc:
