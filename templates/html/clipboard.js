660250117414949814da7ab5ca55b2625c347411
Dimitri van Heesch <doxygen@gmail.com>
2023-11-04 15:13:04 +0100

    Replace obsolete $(document).ready(...) syntax (part 2).

--- templates/html/clipboard.js
+++ templates/html/clipboard.js
@@ -32,7 +32,7 @@ let clipboard_icon = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24
 let clipboard_successIcon = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M9 16.17L4.83 12l-1.42 1.41L9 19 21 7l-1.41-1.41L9 16.17z"/></svg>`
 let clipboard_successDuration = 1000
 
-$(document).ready(function() {
+$(function() {
   if(navigator.clipboard) {
     const fragments = document.getElementsByClassName("fragment")
     for(const fragment of fragments) {
