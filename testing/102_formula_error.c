3cdda07db88fbf59cc4e3faab6ca595536504d60
Ralf Habacker <ralf.habacker@freenet.de>
2024-01-03 15:02:51 +0100

    Add test for failed generation of formulas

---
+++ testing/102_formula_error.c
@@ -0,0 +1,12 @@
+// objective: test failing \f$, \f[, \f], \f{, and \f} commands
+// check: html/_formulas.tex
+
+/** @mainpage
+Here are some formulas:
+-# inline \f$ \e \f$
+-# Unnumbered formula:
+   \f[ \e \f]
+-# Formula in different environment
+  \f{eqnarray*}{ \e
+ \f}
+*/
