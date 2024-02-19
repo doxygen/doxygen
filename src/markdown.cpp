40614b13fc796bb6af6986c881ddc23d7fa047de
jnkvca <122721263+jnkvca@users.noreply.github.com>
2024-02-19 18:59:42 +0100

    Corrected check for Markdown style links to not change previous Doxygen behavior

--- src/markdown.cpp
+++ src/markdown.cpp
@@ -1529,7 +1529,7 @@ int Markdown::Private::processLink(const std::string_view data,size_t offset)
     }
     else if ((lp=link.find('#'))!=-1 || link.find('/')!=-1 || link.find('.')!=-1)
     { // file/url link
-      if (lp!=-1 && !isURL(link) && Config_getEnum(MARKDOWN_ID_STYLE)==MARKDOWN_ID_STYLE_t::GITHUB)
+      if (lp==0 || (lp>0 && !isURL(link) && Config_getEnum(MARKDOWN_ID_STYLE)==MARKDOWN_ID_STYLE_t::GITHUB))
       {
         out+="@ref \"";
         out+=AnchorGenerator::addPrefixIfNeeded(link.mid(lp+1).str());
