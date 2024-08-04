a76ef196c968ac3aa1ceee72c4c799f850898c45
Jens Kallup <paule32.jk@gmail.com>
2024-08-04 13:10:04 +0200

    Update main.cpp
    restore to original code

--- src/main.cpp
+++ src/main.cpp
@@ -18,59 +18,6 @@
 
 #include "doxygen.h"
 
-/**
- * \brief code added by paule32, to check for chm viewer instances under MS-Windows 10, 11 is open.
- * \details
- * If you compile a CHM HTML Project, you would not be inform, if the WebViewer instance is open.
- * And as such, Doxygen produce undefined behavoiur. To workaround this, you get a message where
- * you can choose, if you would work through, or exit the Doxygen App.
- *
- * 1. CreateToolhelp32Snapshot:
- *    This funtion create a SnapShot of all running processes in the System.
- * 2. PROCESSENTRY32:
- *    A structure, they holds informations about the process.
- * 3. Process32First, and Process32Next:
- *    This function's will be used to iterate through all processes that are in the SnapShot.
- * 4. _tcscmp:
- *    A function, that compares two string's. In this case, the given string will be check, if a process
- *    with this name is available.
- */
-#if defined(_WIN32) || defined(_WIN64)
-# include <windows.h>
-# include <tlhelp32.h>
-# include <tchar.h>
-# include <stdio.h>
-# include <iostream>
-# include <string>
-using namespace std;
-
-BOOL IsProcessRunning(const TCHAR *processName, DWORD *processId) {
-    BOOL exists = FALSE;
-    HANDLE snapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
-    PROCESSENTRY32 processEntry = {0};
-    processEntry.dwSize = sizeof(PROCESSENTRY32);
-    if (Process32First(snapshot, &processEntry)) {
-        do {
-            if (_tcscmp(processEntry.szExeFile, processName) == 0) {
-                *processId = processEntry.th32ProcessID;
-                exists = TRUE;
-                break;
-            }
-        }
-        while (Process32Next(snapshot, &processEntry));
-    }
-    CloseHandle(snapshot);
-    return exists;
-}
-void TerminateProcessById(DWORD processId) {
-    HANDLE processHandle = OpenProcess(PROCESS_TERMINATE, FALSE, processId);
-    if (processHandle != NULL) {
-        TerminateProcess(processHandle, 0);
-        CloseHandle(processHandle);
-    }
-}
-#endif
-
 /*! \file
  *  \brief main entry point for doxygen
  *
@@ -83,18 +30,6 @@ void TerminateProcessById(DWORD processId) {
  */
 int main(int argc,char **argv)
 {
-  /**
-    * \brief code added by paule32, to check for chm viewer instances under
-    *        MS-Windows 10, 11 is open ...
-    */
-  #if defined(_WIN32) || defined(_WIN64)
-  const TCHAR *processName = _T("msedgewebview2.exe");
-  DWORD processId = 0;
-  if (IsProcessRunning(processName, &processId)) {
-      cout << "The HTML Viewer is already open.\nTry to close it..." << endl;
-      TerminateProcessById(processId);
-  }
-  #endif
   initDoxygen();
   readConfiguration(argc,argv);
   checkConfiguration();
