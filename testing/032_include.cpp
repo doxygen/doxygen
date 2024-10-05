1822879cdf4a7a08bd75870ea9257bd2946e56ca
Dimitri van Heesch <doxygen@gmail.com>
2024-10-05 15:54:28 +0200

    Quick fix for failing test 032

--- testing/032_include.cpp
+++ testing/032_include.cpp
@@ -1,5 +1,6 @@
 // objective: test the \include and \includelineno commands
 // check: indexpage.xml
+// check: another_page.xml
 // config: STRIP_CODE_COMMENTS=NO
 
 /** \mainpage
@@ -7,7 +8,10 @@
  *  \include example_test.cpp
  *  More text.
  *  \include{lineno} example_test.cpp
- *  Same example with stipped comments
+ */
+
+/** \page another_page Example with stipped comments
+ *  Here is the same example with stipped comments
  *  \include{lineno,strip} example_test.cpp
  *  End.
  */
