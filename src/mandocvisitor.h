88aa9d22af61076addeb6fccec7178c8ecdb03cc
Duncan Roe <duncan_roe@optusnet.com.au>
2024-12-13 15:55:15 +1100

    issue #11271 man page verbatim changing \" to \'
    6c5b9295509e introduced ManDocVisitor::filter() which corrects verbatim \\
    but converts \" to \' which is inappropriate for case DocVerbatim::Verbatim
    and at least one other case.
    
    This commit adds a bool to filter() so one can tune which calls to it
    do the \" conversion.
    
    I have no idea which other calls should not do the \" conversion so for now
    that conversion is turned off for all calls by commenting it out.
    
    Fixes: 6c5b9295509e: Issue with spacing around <programlisting>
    Signed-off-by: Duncan Roe <duncan_roe@optusnet.com.au>

--- src/mandocvisitor.h
+++ src/mandocvisitor.h
@@ -117,7 +117,7 @@ class ManDocVisitor : public DocVisitor
     // helper functions
     //--------------------------------------
 
-    void filter(const QCString &str);
+    void filter(const QCString &str, bool fixquotes);
 
     //--------------------------------------
     // state variables
