a3bd787d762277baef5228c3fd5ea4f591df6ad7
Dimitri van Heesch <doxygen@gmail.com>
2025-05-03 15:32:20 +0200

    issue #11557 @tableofcontents shows <tt> tags for MarkDown headings with inline code
    Also fix issue with search results showing HTML tags

--- templates/html/search.js
+++ templates/html/search.js
@@ -607,6 +607,12 @@ function createResults(resultsPath) {
     elem.setAttribute('className',attr);
   }
 
+  const decodeHtml = (html) => {
+    const txt = document.createElement("textarea");
+    txt.innerHTML = html;
+    return txt.value;
+  };
+
   const results = document.getElementById("SRResults");
   results.innerHTML = '';
   searchData.forEach((elem,index) => {
@@ -620,7 +626,7 @@ function createResults(resultsPath) {
     srLink.setAttribute('id','Item'+index);
     setKeyActions(srLink,'return searchResults.Nav(event,'+index+')');
     setClassAttr(srLink,'SRSymbol');
-    srLink.innerHTML = elem[1][0];
+    srLink.innerHTML = decodeHtml(elem[1][0]);
     srEntry.appendChild(srLink);
     if (elem[1].length==2) { // single result
       if (elem[1][1][0].startsWith('http://') || elem[1][1][0].startsWith('https://')) { // absolute path
@@ -636,7 +642,7 @@ function createResults(resultsPath) {
       }
       const srScope = document.createElement('span');
       setClassAttr(srScope,'SRScope');
-      srScope.innerHTML = elem[1][1][2];
+      srScope.innerHTML = decodeHtml(elem[1][1][2]);
       srEntry.appendChild(srScope);
     } else { // multiple results
       srLink.setAttribute('href','javascript:searchResults.Toggle("SR_'+id+'")');
@@ -658,7 +664,7 @@ function createResults(resultsPath) {
         } else {
          srChild.setAttribute('target','_blank');
         }
-        srChild.innerHTML = elem[1][c+1][2];
+        srChild.innerHTML = decodeHtml(elem[1][c+1][2]);
         srChildren.appendChild(srChild);
       }
       srEntry.appendChild(srChildren);
