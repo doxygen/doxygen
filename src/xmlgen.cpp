bfe3edb2515e215bd984b650036041812c8b6778
Dimitri van Heesch <doxygen@gmail.com>
2025-05-14 21:45:29 +0200

    Reformat code to match doxygen code styling

--- src/xmlgen.cpp
+++ src/xmlgen.cpp
@@ -583,36 +583,46 @@ static QCString memberOutputFileBase(const MemberDef *md)
   return md->getOutputFileBase();
 }
 
-static bool stripKeyword(QCString& str, const char* keyword, bool needSpace) {
-  bool found = false;
+// Removes a keyword from a given string
+// @param str string from which to strip the keyword
+// @param needSpace true if spacing is required around the keyword
+// @return true if the keyword was removed, false otherwise
+static bool stripKeyword(QCString& str, const char *keyword, bool needSpace)
+{
+  bool found      = false;
   int searchStart = 0;
-  size_t len = strlen(keyword);
-  int searchEnd = (int) str.size();
-  while (searchStart < searchEnd) {
+  int len         = static_cast<int>(strlen(keyword));
+  int searchEnd   = static_cast<int>(str.size());
+  while (searchStart<searchEnd)
+  {
     int index = str.find(keyword, searchStart);
-    if (index == -1) {
-      break;
+    if (index==-1)
+    {
+      break; // no more occurrences found
     }
     int end = index + len;
-    if (needSpace) {
-      // Either at the start of the string or preceded by a space.
-      if (index > 0 && str[index - 1] != ' ') {
+    if (needSpace)
+    {
+      if ((index>0        && str[index-1]!=' ') ||  // at the start of the string or preceded by a space, or
+          (end!=searchEnd && str[end]    !=' ')     // at the end of the string or followed by a space.
+         )
+      {
         searchStart = end;
-        continue;
-      }
-      // Either at the end of the string or followed by a space.
-      if (end != searchEnd && str[end] != ' ') {
-        searchStart = end;
-        continue;
+        continue; // no a standalone word
       }
     }
-    if (needSpace && index > 0) {
-      str.remove(index - 1, len + 1);
-      searchEnd -= (len + 1);
-    } else if (needSpace && end < searchEnd) {
-      str.remove(index, len + 1);
-      searchEnd -= (len + 1);
-    } else {
+    if (needSpace && index>0) // strip with space before keyword
+    {
+      str.remove(index-1, len+1);
+      searchEnd -= (len+1);
+    }
+    else if (needSpace && end<searchEnd) // strip with space after string starting with keyword
+    {
+      str.remove(index, len+1);
+      searchEnd -= (len+1);
+    }
+    else // strip just keyword
+    {
       str.remove(index, len);
       searchEnd -= len;
     }
@@ -727,13 +737,16 @@ static void generateXMLForMember(const MemberDef *md,TextStream &ti,TextStream &
     {
       typeStr=argsStr.mid(i+2).stripWhiteSpace();
       argsStr=argsStr.left(i).stripWhiteSpace();
-      if (stripKeyword(typeStr, "override", true)) {
+      if (stripKeyword(typeStr, "override", true))
+      {
         argsStr += " override";
       }
-      if (stripKeyword(typeStr, "final", true)) {
+      if (stripKeyword(typeStr, "final", true))
+      {
         argsStr += " final";
       }
-      if (stripKeyword(typeStr, "=0", false)) {
+      if (stripKeyword(typeStr, "=0", false))
+      {
         argsStr += "=0";
       }
       i=defStr.find("auto ");
