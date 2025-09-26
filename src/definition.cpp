888a2f5edecfa511e27ab18c082999b848b9ec41
artyom-fedosov <artjomsfedosovs2@gmail.com>
2025-09-26 17:40:13 +0300

    refactor(definition): replace manual char conversion with std::toupper

--- src/definition.cpp
+++ src/definition.cpp
@@ -1572,13 +1572,9 @@ static QCString abbreviate(const QCString &s,const QCString &name)
     stripWord(result,str);
   }
 
-  // capitalize first word
-  if (!result.isEmpty())
-  {
-    char c=result[0];
-    if (c>='a' && c<='z') c+='A'-'a';
-    result[0]=c;
-  }
+  // capitalize first character
+  if (!result.isEmpty()) result[0] = static_cast<char>(std::toupper(result[0]));
+
   return result;
 }
 
