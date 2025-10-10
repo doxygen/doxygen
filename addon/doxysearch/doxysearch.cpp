be0d03a6a825f60c6958ef7f95552132dd367877
artyom-fedosov <artjomsfedosovs2@gmail.com>
2025-10-10 22:12:03 +0300

    refactor(doxysearch): modernize dirExists function

--- addon/doxysearch/doxysearch.cpp
+++ addon/doxysearch/doxysearch.cpp
@@ -23,6 +23,7 @@
 #include <fstream>
 #include <string>
 #include <algorithm>
+#include <filesystem>
 
 // Xapian includes
 #include <xapian.h>
@@ -48,25 +49,10 @@
                     ((x)>='A' && (x)<='F')?((x)-'A'+10):-1)
 
 
-bool dirExists(const std::string& dirName)
+bool dirExists(const std::string &dirname)
 {
-#ifdef _WIN32
-  DWORD ftyp = GetFileAttributesA(dirName.c_str());
-  if (ftyp == INVALID_FILE_ATTRIBUTES)
-    return false;  //something is wrong with your path!
-
-  if (ftyp & FILE_ATTRIBUTE_DIRECTORY)
-    return true;   // this is a directory!
-#else
-  struct stat sb;
-
-  if (stat(dirName.c_str(), &sb)==0 && S_ISDIR(sb.st_mode))
-  {
-    return true;
-  }
-#endif
-
-  return false;
+  std::error_code ec;
+  return std::filesystem::is_directory(dirname, ec);
 }
 
 
