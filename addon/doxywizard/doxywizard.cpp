ee31087f39ff4e0efb2e3df9bd412588fe560887
artyom-fedosov <artjomsfedosovs2@gmail.com>
2025-10-13 18:30:18 +0300

    fix(doxywizard): restore AA_EnableHighDpiScaling for Qt < 6

--- addon/doxywizard/doxywizard.cpp
+++ addon/doxywizard/doxywizard.cpp
@@ -807,6 +807,16 @@ void MainWindow::outputLogFinish()
 //-----------------------------------------------------------------------
 int main(int argc,char **argv)
 {
+#if QT_VERSION < QT_VERSION_CHECK(6, 0, 0)
+  static const char ENV_VAR_QT_DEVICE_PIXEL_RATIO[] = "QT_DEVICE_PIXEL_RATIO";
+  if (!qEnvironmentVariableIsSet(ENV_VAR_QT_DEVICE_PIXEL_RATIO)
+      && !qEnvironmentVariableIsSet("QT_AUTO_SCREEN_SCALE_FACTOR")
+      && !qEnvironmentVariableIsSet("QT_SCALE_FACTOR")
+      && !qEnvironmentVariableIsSet("QT_SCREEN_SCALE_FACTORS")) {
+    QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+  }
+#endif
+
   QApplication a(argc,argv);
   int locArgc = argc;
 
