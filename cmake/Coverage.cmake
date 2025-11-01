fa6462b679cb146ffbae41c4cd9190454af8eabb
artyom-fedosov <artjomsfedosovs2@gmail.com>
2025-11-01 16:31:24 +0200

    Revert "refactor: replace PROJECT_SOURCE_DIR to TOP"

--- cmake/Coverage.cmake
+++ cmake/Coverage.cmake
@@ -13,14 +13,14 @@ if(enable_coverage)
 	COMMAND ${LCOV_PATH} --rc lcov_branch_coverage=1 --directory . --capture --output-file cov.info
 	COMMAND ${LCOV_PATH} --rc lcov_branch_coverage=1 --remove cov.info '*/c++/*' '*/_ctype.h' '*/generated_src/*' --output-file cov.info.cleaned
 	COMMAND ${CMAKE_COMMAND} -Dsearch=${PROJECT_BINARY_DIR}
-	                         -Dreplace=${TOP}
+	                         -Dreplace=${PROJECT_SOURCE_DIR}
 				 -Dsrc=cov.info.cleaned
 				 -Ddst=cov.info.final
-				 -P ${TOP}/cmake/SearchReplace.cmake
+				 -P ${PROJECT_SOURCE_DIR}/cmake/SearchReplace.cmake
 	COMMAND ${GENHTML_PATH} --rc genhtml_branch_coverage=1
 	                        --function-coverage --branch-coverage
 	                        --title "Doxygen Coverage Report" --num-spaces 2
-				--legend --prefix ${TOP} --demangle-cpp
+				--legend --prefix ${PROJECT_SOURCE_DIR} --demangle-cpp
 				--output-directory cov_output cov.info.final
         COMMAND ${CMAKE_COMMAND} -E remove cov.info cov.info.cleaned cov.info.final
 	WORKING_DIRECTORY ${PROJECT_BINARY_DIR}
