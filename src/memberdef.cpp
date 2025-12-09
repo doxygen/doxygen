edfb98122c1ddd4f1ecee8feb3369f6000b306cb
albert-github <albert.tests@gmail.com>
2025-12-09 16:31:24 +0100

    issue #11891 noway to control newline generation in html Variable Documentation
    The initial part of the initializing of a variable i.e. the part before the `=` sign was omitted from the initializer and hence a bit a strange indentation.
    - `scanner.l` add the initial part as white space
    -`memberdef.cpp` remove the initial common whitespace of all initializer lines
    - `memberdef.cpp` in `setAnchor` the extra white space has to be added to remain compatible (otherwise the anchor will change, tests 59, 72, 104, 105)

--- src/memberdef.cpp
+++ src/memberdef.cpp
@@ -4395,12 +4395,11 @@ void MemberDefImpl::setAnchor()
   if (!m_args.isEmpty()) memAnchor+=m_args;
   if (m_memSpec.isAlias()) // this is for backward compatibility
   {
-    memAnchor.prepend(" = "+m_initializer);
+    memAnchor.prepend(" =  "+m_initializer);
   }
   memAnchor.prepend(definition()); // actually the method name is now included
             // twice, which is silly, but we keep it this way for backward
             // compatibility.
-
 
   // include number of template arguments as well,
   // to distinguish between two template
@@ -4549,6 +4548,31 @@ void MemberDefImpl::setInitializer(const QCString &initializer)
   while (p>=0 && isspace(static_cast<uint8_t>(m_initializer.at(p)))) p--;
   m_initializer=m_initializer.left(p+1);
   m_initLines=m_initializer.contains('\n');
+  QCString s(m_initializer);
+  const char *data = s.data();
+  size_t i = 0;
+  size_t col = 0;
+  size_t size = s.length();
+  m_initializer.clear();
+  while (i<size)
+  {
+    char c = data[i++];
+    switch(c)
+    {
+      case '\n':
+        m_initializer+=c;
+        col=0;
+        break;
+      case ' ':
+        if (col < indent) col++;
+        else m_initializer+=c;
+        break;
+      default:
+        m_initializer+=c;
+        break;
+    }
+  }
+
   //printf("%s::setInitializer(%s)\n",qPrint(name()),qPrint(m_initializer));
 }
 
