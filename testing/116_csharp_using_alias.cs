b756154c0475eb4d4dcc8cbf6653a84f859bb615
nopeless <38830903+nopeless@users.noreply.github.com>
2026-02-08 04:12:20 +0000

    Implement C# using alias tests 116

---
+++ testing/116_csharp_using_alias.cs
@@ -0,0 +1,53 @@
+// objective: check C# using alias
+// config: EXTRACT_ALL=YES
+// check: class_test_app_1_1_test_class.xml
+
+namespace N {
+  public class A {}
+  public class B {}
+  public class C {}
+
+  namespace Collections {
+    public class Dictionary<TKey, TValue> {}
+    public class List<T> {}
+  }
+}
+
+using Foo = N.A;
+using Bar = N.Collections.Dictionary<string, N.Collections.List<N.A>>;
+
+using GlobalFoo = global::N.B;
+using GlobalBar = N.Collections.Dictionary<string, N.Collections.List<N.B>>;
+
+namespace TestApp
+{
+    public class Outer {
+        public class Nested {}
+    }
+}
+
+namespace TestApp
+{
+    using InnerFoo = N.C;
+    using InnerBar = N.Collections.Dictionary<string, N.Collections.List<N.C>>;
+
+    using NestedAlias = TestApp.Outer.Nested;
+
+    public class TestClass : Bar
+    {
+        public Foo myFoo;
+        public Bar myBar;
+
+        public InnerFoo myInnerFoo;
+        public InnerBar myInnerBar;
+
+        public GlobalFoo myGlobalFoo;
+        public GlobalBar myGlobalBar;
+
+        public NestedAlias myNestedAlias;
+
+        public Foo fooMethod(Bar b) { return null; }
+        public InnerFoo innerFooMethod(InnerBar b) { return null; }
+        public GlobalFoo globalFooMethod(GlobalBar b) { return null; }
+    }
+}
