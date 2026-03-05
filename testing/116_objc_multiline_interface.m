3034a9674f0c422ce82ba3ec0bf4d7243e168fb0
Dawid Malecki <dawid.malecki21@gmail.com>
2026-03-05 16:46:47 +0100

    test

--- testing/116_objc_multiline_interface.m
+++ testing/116_objc_multiline_interface.m
@@ -1,16 +1,16 @@
 // objective: test Objective-C interface with generics and base class on separate line
-// check: interface_r_c_t_generic_delegate_splitter.xml
+// check: interface_foo.xml
 // config: EXTENSION_MAPPING = h=Objective-C
 
 /**
- * A generic delegate splitter.
+ * A generic container.
  */
-@interface RCTGenericDelegateSplitter<DelegateT>
+@interface Foo<T>
     : NSObject
 
-/** add a delegate */
-- (void)addDelegate:(DelegateT)delegate;
-/** remove a delegate */
-- (void)removeDelegate:(DelegateT)delegate;
+/** add an item */
+- (void)addItem:(T)item;
+/** remove an item */
+- (void)removeItem:(T)item;
 
 @end
