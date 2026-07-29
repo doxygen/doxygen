db26e1e0c9089378da8992fe696deabfdb556d7e
Dimitri van Heesch <doxygen@gmail.com>
2026-07-29 13:32:16 +0200

    issue #5940 Look for [[deprecated]] markers (Origin: bugzilla #761131)

--- testing/122_deprecated.cpp
+++ testing/122_deprecated.cpp
@@ -18,12 +18,47 @@ struct Logger {
     /*! new logger constructor */
     Logger(int level) {}
 
-    [[ nodiscard, deprecated ( "Use write() instead" ) ]]
-    void log() {}
+    // ----- various ways to deprecate a function
+    //
+    // A. attribute present with documentation
+    // B. attribute present without documentation
+    // C. @deprecated present
+    //
+    // Different combinations of the above (A and B are mutually exclusive)
+    // 1. A
+    // 2. B
+    // 3. C
+    // 4. C + B
+    // 5. B + C
+    // 6. C + A
+    // 7. A + C
 
-    /*! @deprecated Use write() instead */
+    [[ nodiscard, deprecated ( "\e Use `write()` *instead*" ) ]]
+    bool log_old1() {}
+
     [[deprecated]]
-    void log_old() {}
+    void log_old2() {}
+
+    /*! @deprecated Use `write()` instead */
+    void log_old3() {}
+
+    /*! @deprecated Use `write()` instead */
+    [[deprecated]]
+    void log_old4() {}
+
+    [[deprecated]]
+    /*! @deprecated Use `write()` instead */
+    void log_old5() {}
+
+    /*! @deprecated Use `write()` instead */
+    [[deprecated("Second deprecation message")]]
+    void log_old6() {}
+
+    [[deprecated("Use `write()` instead")]]
+    /*! @deprecated Second deprecation message */
+    void log_old7() {}
+
+    // ------
 
     /*! new function */
     void write() {}
